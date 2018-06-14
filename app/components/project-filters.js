a3b38ab08825bd4f24584e1afc70f5ff1002287c
Andy Cochran <acochran@planning.nyc.gov>
2018-06-14 17:04:36 -0400

    add project-filters component, UI, style

---
+++ app/components/project-filters.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import { argument } from '@ember-decorators/argument';
+
+export default class ProjectFiltersComponent extends Component {
+  @argument closed = true;
+}
