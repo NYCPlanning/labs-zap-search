c9fbdf12b1a08da64212fdea6aac06357b55a3cc
Chris Whong <chris@whong.org>
2018-06-15 12:07:16 -0400

    add projects points map to show-geography

---
+++ app/controllers/show-geography.js
@@ -0,0 +1,80 @@
+import Controller from '@ember/controller';
+import { action, computed} from '@ember-decorators/object';
+import carto from 'cartobox-promises-utility/utils/carto';
+
+export default class ShowGeographyController extends Controller {
+  transformRequest(url) {
+    window.XMLHttpRequest = window.XMLHttpRequestNative;
+    return { url };
+  }
+
+  projectCentroidsTileTemplate = null
+
+  projectCentroidsLayer = {
+    id: 'project-centroids-circle',
+    type: 'circle',
+    'source-layer': 'project-centroids',
+    paint: {
+      'circle-radius': { stops: [[10, 3], [15, 4]] },
+      'circle-color': '#ae561f',
+      'circle-opacity': 1,
+      'circle-stroke-width': { stops: [[10, 1], [15, 2]] },
+      'circle-stroke-color': '#FFFFFF',
+    },
+  }
+
+  @computed('projectCentroidsTileTemplate')
+  get projectCentroidsSource() {
+    return {
+      type: 'vector',
+      tiles: [this.get('projectCentroidsTileTemplate')],
+    }
+  }
+
+  @action
+  handleMapLoad(bblFeatureCollection, map) {
+    window.map = map;
+    this.set('map', map)
+    // initiate carto handshake
+    const sourceLayers = [{
+      id: 'project-centroids',
+      sql: 'SELECT * FROM project_centroids',
+    }];
+
+    carto.getVectorTileTemplate(sourceLayers)
+      .then((tileTemplate) => {
+        this.set('projectCentroidsTileTemplate', tileTemplate)
+      });
+  }
+
+  @action
+  handleMapMove(e) {
+    // show a pointer cursor if there is a feature under the mouse pointer
+    const map = this.get('map');
+    const Feature = map.queryRenderedFeatures(
+      e.point,
+      { layers: ['project-centroids-circle']}
+    )[0];
+
+    if (Feature) {
+      map.getCanvas().style.cursor = 'pointer';
+    } else {
+      map.getCanvas().style.cursor = 'default';
+    }
+  }
+
+  @action
+  handleMapClick(e) {
+    const map = this.get('map');
+    const Feature = map.queryRenderedFeatures(
+      e.point,
+      { layers: ['project-centroids-circle']}
+    )[0];
+
+    if (Feature) {
+      const projectid = Feature.properties.projectid;
+      // set the route of the app!
+      this.transitionToRoute(`/projects/${projectid}`);
+    }
+  }
+}
