8e79031da565b27a95b51642bd8c38751dfb3818
Chris Whong <chris@whong.org>
2018-06-15 12:51:09 -0400

    WIP geography-view-results

--- app/routes/show-geography.js
+++ app/routes/show-geography.js
@@ -1,7 +1,14 @@
 import Route from '@ember/routing/route';
+import fetch from 'fetch';
 
 export default class ShowGeographyRoute extends Route {
   model({ id }) {
-    return this.store.findRecord('geography', id, { include: 'projects' });
+    let [ cdBoro, cdNumber ] = id.split('-');
+
+    cdNumber = ("0" + cdNumber).slice(-2);
+
+    const cdParam = `${cdBoro}${cdNumber}`;
+    return fetch(`https://zap-api.planninglabs.nyc/projects?community-district=${cdParam}`)
+      .then((projects) => { projects });
   }
 }
