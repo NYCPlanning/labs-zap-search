e70213526199eb9ed8bcaffd2b02cda7ea074269
Chris Whong <chris@whong.org>
2018-06-18 13:41:58 -0400

    add simple tooltip component to actions

---
+++ app/components/icon-tooltip.js
@@ -0,0 +1,9 @@
+import Component from '@ember/component';
+import { argument } from '@ember-decorators/argument';
+
+export default class InfoTooltip extends Component{
+  tagName = 'span'
+
+  @argument
+  tip = ''
+}
