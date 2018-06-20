3469ecc1c20613c70f71cb1ce96f391ad463dc33
allthesignals <wilburnforce@gmail.com>
2018-06-20 18:25:22 -0400

    Rename to projects-map-data

--- tests/integration/components/projects-map-data-test.js
+++ tests/integration/components/projects-map-data-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | projects-list-map', function(hooks) {
+module('Integration | Component | projects-map-data', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{projects-list-map}}`);
+    await render(hbs`{{projects-map-data}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      {{#projects-list-map}}
+      {{#projects-map-data}}
         template block text
-      {{/projects-list-map}}
+      {{/projects-map-data}}
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
