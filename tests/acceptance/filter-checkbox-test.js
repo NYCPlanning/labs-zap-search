a1f0d9e2a97ce518e274f94d701dfe86ff93c612
trbmcginnis <taylormcgin@gmail.com>
2018-06-21 13:09:50 -0400

    Checkbox, Fillin, and Load More reset pagination tests

--- tests/acceptance/filter-checkbox-test.js
+++ tests/acceptance/filter-checkbox-test.js
@@ -1,19 +1,64 @@
 import { module, test } from 'qunit';
-import { visit, currentURL, click} from '@ember/test-helpers';
+import { visit, currentURL, click, pauseTest, fillIn } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
-
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('Acceptance | filter checkbox', function(hooks) {
   setupApplicationTest(hooks);
+  setupMirage(hooks);
 
-  test('visiting /filter-checkbox', async function(assert) {
+  test('User clicks first project status and it filters', async function(assert) {
+    server.createList('project', 20);
     await visit('/');
-    await click ('.status-checkbox li:first-child a');
-    //await andThen(()=>assert.equal(
+    await click('.status-checkbox li:first-child a');
 
-    	//currentValues=projectFilters.dcp_publicstatus??
-    	//=> find (.'results')
-
-    assert.equal(currentURL(), '/filter-checkbox');
+    assert.equal(currentURL(), '/projects?dcp_publicstatus=Approved%2CCertified%2CUnknown%2CWithdrawn');
   });
+
+  test('User clicks first CEQR Status and it filters', async function(assert) {
+    server.createList('project', 20);
+    await visit('/');
+    await click('.CEQR-checkbox li:first-child a');
+
+    assert.equal(currentURL(), '/projects?dcp_ceqrtype=Type%20I%2CType%20II%2CUnknown');
+  });
+
+    test('User clicks first FEMA Flood Zone status and it filters', async function(assert) {
+    server.createList('project', 20);
+    await visit('/');
+    await click('.FEMA-checkbox li:first-child a');
+
+    assert.equal(currentURL(), '/projects?dcp_femafloodzonea=true');
+  });
+
+    test('User clicks first ULURP status and it filters', async function(assert) {
+    server.createList('project', 20);
+    await visit('/');
+    await click('.ULURP-checkbox li:first-child a');
+    await pauseTest();
+
+    assert.equal(currentURL(), '/projects?dcp_femafloodzonea=true&dcp_ulurp_nonulurp=Non-ULURP');
+  });
+
+  test('User clicks community district box, fills in community district name, selects CD', async function(assert) {
+    server.createList('project', 20);
+    await visit('/');
+    await click('.ember-power-select-multiple-options');
+    await fillIn('.ember-power-select-multiple-options input', 'Brooklyn 1');
+    await click ('.ember-power-select-options li:first-child');
+
+    assert.equal(currentURL(), '/projects?community-districts=BK01');
+  });
+
+    test('Page reloads (pagination reset) when click new filter', async function(assert) {
+    server.createList('project', 20);
+    await visit('/projects?page=2');
+    await click('.status-checkbox li:first-child a');
+    await visit('/projects?dcp_publicstatus=Approved%2CCertified%2CUnknown%2CWithdrawn');
+
+    assert.equal(currentURL(), '/projects?community-districts=BK01');
+  });
+
+
+
 });
