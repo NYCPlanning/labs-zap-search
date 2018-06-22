d0793f74423dc12ad5e96fecfa283fb1fd01f116
allthesignals <wilburnforce@gmail.com>
2018-06-22 12:12:40 -0400

    Better MVT; string actions

--- mirage/serializers/application.js
+++ mirage/serializers/application.js
@@ -8,7 +8,7 @@ export default JSONAPISerializer.extend({
     json.meta = {
       total: 29,
       pageTotal: 29,
-      tiles: ['http://localhost:4200/test-data/tiles/1539.pbf'],
+      tiles: ['http://localhost:4200/test-data/tiles/96.mvt'],
       bounds: [[-73.9916082509177, 40.6824244259472],[-73.8847869770557, 40.8933091086328]],
     };
 
