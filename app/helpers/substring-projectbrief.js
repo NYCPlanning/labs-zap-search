be0ebc34744605dfe556ccf0e42c5d6dbcf6d53a
trbmcginnis <taylormcgin@gmail.com>
2018-06-25 20:24:05 -0400

    Updates on Filter View project list

---
+++ app/helpers/substring-projectbrief.js
@@ -0,0 +1,18 @@
+import { helper } from '@ember/component/helper';
+
+
+function shortenProjectBrief([projectbrief]) {
+if (projectbrief != null && projectbrief.length > 120) {
+    var shortenedString = `${projectbrief.substring(0,120)}...`;
+    return shortenedString;
+  }
+
+  else {
+      return projectbrief;
+  }
+
+}
+
+
+export default helper(shortenProjectBrief);
+
