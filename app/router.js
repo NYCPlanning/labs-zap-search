1166e4f27033b1e74d9b01b16b03ec8e699eaa54
allthesignals <wilburnforce@gmail.com>
2018-07-11 11:11:40 -0400

    Add GA to route transitions

--- app/router.js
+++ app/router.js
@@ -1,7 +1,24 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
+import { inject as service } from '@ember/service';
+import { scheduleOnce } from '@ember/runloop';
+import { get } from '@ember/object';
 
 const Router = EmberRouter.extend({
+  metrics: service(),
+  didTransition(...args) {
+    this._super(...args);
+    this._trackPage();
+  },
+
+  _trackPage() {
+    scheduleOnce('afterRender', this, () => {
+      const page = this.get('url');
+      const title = this.getWithDefault('currentRouteName', 'unknown');
+      get(this, 'metrics').trackPage({ page, title });
+    });
+  },
+
   location: config.locationType,
   rootURL: config.rootURL
 });
