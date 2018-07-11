4343ada3867d5a473a0ce0635374b92bcc30fcd2
Chris Whong <chris@whong.org>
2018-07-10 20:51:39 -0400

    add babel, use qs instead of query-string

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    'ember-cli-babel': {
+      includePolyfill: true,
+    },
     'ember-math-helpers': {
       only: ['add'],
     },
