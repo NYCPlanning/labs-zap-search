a20ed27c9f604e72cc7c2d88c1411c1a0fff1bb4
allthesignals <wilburnforce@gmail.com>
2018-07-11 13:06:48 -0400

    Re-add redirect

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({
-});
+export default class ApplicationRoute extends Route {
+  beforeModel(transition) {
+    // load the projects view by default
+    if (transition.intent.url === '/') {
+      this.transitionTo('show-geography');
+    }
+  }
+}
