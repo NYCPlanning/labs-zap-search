afdb272e13151f875848f3c3b29d1e3467f23159
Andy Cochran <acochran@planning.nyc.gov>
2018-07-13 16:09:27 -0400

    fix test, revise disclaimer

--- tests/integration/components/map-info-box-test.js
+++ tests/integration/components/map-info-box-test.js
@@ -3,16 +3,16 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | map-legend', function(hooks) {
+module('Integration | Component | map-info-box', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{map-legend}}`);
+    await render(hbs`{{map-info-box}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.equal(this.element.textContent.trim().replace(/\s/g, ''), 'FiledInPublicReviewCompletedDisclaimer');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | map-legend', function(hooks) {
       {{/map-info-box}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(this.element.textContent.trim().replace(/\s/g, ''), 'FiledInPublicReviewCompletedDisclaimertemplateblocktext');
   });
 });
