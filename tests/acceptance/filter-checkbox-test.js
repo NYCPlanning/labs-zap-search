b19bc8f46034d07097fd70f53e63da432308b3e7
Chris Whong <chris@whong.org>
2018-07-12 20:12:33 -0400

    fix broken tests

--- tests/acceptance/filter-checkbox-test.js
+++ tests/acceptance/filter-checkbox-test.js
@@ -18,7 +18,7 @@ module('Acceptance | filter checkbox', function(hooks) {
     await visit('/');
     await click('.stage-checkbox li:first-child a');
 
-    assert.equal(currentURL().includes('Completed%2CIn%20Public%20Review'), true);
+    assert.equal(currentURL().includes('In%20Public%20Review'), true);
   });
 
   test('User clicks first CEQR Status and it filters', async function(assert) {
@@ -52,7 +52,7 @@ module('Acceptance | filter checkbox', function(hooks) {
     await fillIn('.filter-section-community-district .ember-power-select-multiple-options input', 'Brooklyn 1');
     await click ('.ember-power-select-options li:first-child');
 
-    assert.equal(currentURL(), '/projects?community-districts=BK01');
+    assert.equal(currentURL(), '/projects?applied-filters=community-districts%2Cdcp_publicstatus&community-districts=BK01');
   });
 
   skip('Page reloads (pagination reset) when click new filter', async function(assert) {
@@ -93,7 +93,7 @@ module('Acceptance | filter checkbox', function(hooks) {
     await visit('/projects');
     await click('.filter-section-fema-flood-zone .switch-paddle');
 
-    assert.equal(currentURL(), '/projects?applied-filters=action-types%2Ccommunity-districts%2Cdcp_femafloodzonea%2Cdcp_femafloodzonecoastala%2Cdcp_femafloodzoneshadedx%2Cdcp_femafloodzonev%2Cdcp_publicstatus');
+    assert.equal(currentURL(), '/projects?applied-filters=dcp_femafloodzonea%2Cdcp_femafloodzonecoastala%2Cdcp_femafloodzoneshadedx%2Cdcp_femafloodzonev%2Cdcp_publicstatus');
     await click('.filter-section-fema-flood-zone .switch-paddle');
 
     assert.equal(currentURL(), '/projects');
