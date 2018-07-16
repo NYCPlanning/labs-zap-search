04ce30da55c68b9df54f5fc1fb9e20f5a2b2fd83
Andy Cochran <acochran@planning.nyc.gov>
2018-07-16 15:52:15 -0400

    add general feedback email address

--- app/components/labs-site-header.js
+++ app/components/labs-site-header.js
@@ -4,5 +4,12 @@ import { classNames } from '@ember-decorators/component';
 
 @classNames('site-header')
 export default class LabsSiteHeaderComponent extends Component {
-  @argument closed = true;
+  @argument
+  closed = true;
+
+  @argument
+  feedbackTooltip = `
+    <p>Please report data errors by using the “Report Data Issue” form on each particular project's page.</p>
+    <p class="no-margin">For general feedback about this application, email <strong><a href="mailto:ZAP_Feedback_DL@planning.nyc.gov">ZAP_Feedback_DL@planning.nyc.gov</a></strong>.</p>
+  `;
 }
