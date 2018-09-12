199e6c37b114bef19606696fefb4f6654282ab5d
trbmcginnis <taylormcgin@gmail.com>
2018-09-12 15:45:32 -0400

    change conditions for cpc report link

--- app/routes/show-project.js
+++ app/routes/show-project.js
@@ -6,6 +6,46 @@ export default class ShowProjectRoute extends Route {
     return this.store.findRecord('project', id, { reload: true });
   }
 
+  afterModel(...args) {
+    const [model] = args;
+
+    model.actions.forEach((cpcAction) => {
+      if (cpcAction.statuscode === 'Approved') {
+        if (cpcAction.actioncode === 'BD'
+              || cpcAction.actioncode === 'HI' // nonulurp
+              || cpcAction.actioncode === 'HK'
+              || cpcAction.actioncode === 'HN'
+              || cpcAction.actioncode === 'NP'
+              || cpcAction.actioncode === 'PX'
+              || cpcAction.actioncode === 'ZR'
+              || cpcAction.actioncode === 'BF' // ulurp
+              || cpcAction.actioncode === 'GF'
+              || cpcAction.actioncode === 'HA'
+              || cpcAction.actioncode === 'HD'
+              || cpcAction.actioncode === 'HO'
+              || cpcAction.actioncode === 'HP'
+              || cpcAction.actioncode === 'HU'
+              || cpcAction.actioncode === 'MC'
+              || cpcAction.actioncode === 'ML'
+              || cpcAction.actioncode === 'MM'
+              || cpcAction.actioncode === 'PC'
+              || cpcAction.actioncode === 'PE'
+              || cpcAction.actioncode === 'PI'
+              || cpcAction.actioncode === 'PP'
+              || cpcAction.actioncode === 'PQ'
+              || cpcAction.actioncode === 'PS'
+              || cpcAction.actioncode === 'RS'
+              || cpcAction.actioncode === 'SD'
+              || cpcAction.actioncode === 'ZM'
+              || cpcAction.actioncode === 'ZS') {
+          cpcAction.has_cpcReport = true;
+        }
+      }
+    });
+
+    super.afterModel(...args);
+  }
+
   @action
   error() {
     this.transitionTo('not-found', 'not-found');
