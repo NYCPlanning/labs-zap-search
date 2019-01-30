6c7ebafc7758f0830033d6a65679722f596cda99
Matt Gardner <wilburnforce@gmail.com>
2019-01-30 09:43:27 -0500

    Remove placeholder data

--- app/components/project-milestone.js
+++ app/components/project-milestone.js
@@ -161,12 +161,6 @@ export default class ProjectMilestoneComponent extends Component {
 
   classNameBindings = ['getClassNames'];
 
-  milestoneLinks = [
-    '070194_DOB.pdf',
-    '070194A_HPD.pdf',
-    '070194_Q04.pdf',
-  ];
-
   @argument
   milestone
 
