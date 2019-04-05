94f001aad4384bc284a579914325a7c44f452d26
allthesignals <wilburnforce@gmail.com>
2019-04-05 13:30:41 -0400

    Improve mapbox-gl-stub

--- tests/helpers/mapbox-gl-stub.js
+++ tests/helpers/mapbox-gl-stub.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import hbs from 'htmlbars-inline-precompile';
 import deepmerge from 'deepmerge';
+import { classNames } from '@ember-decorators/component';
 
 const defaultMapboxEventStub = {
   target: {
@@ -11,9 +12,14 @@ const defaultMapboxEventStub = {
     getCanvas: () => ({
       style: {},
     }),
+    getStyle: () => ({
+      sources: {},
+      layers: {},
+    }),
   },
 };
 
+@classNames('mapbox-gl')
 export class MapboxGl extends Component {
   mapLoaded = () => {}
 
@@ -23,44 +29,65 @@ export class MapboxGl extends Component {
     // mapLoaded is an ember-mapbox-gl convenience
     // which specifically sends out the "target"
     // as the map instance
-    this.mapLoaded(this.mapboxEventStub.target);
+    const currentMock = deepmerge(defaultMapboxEventStub, this.testContext.mapboxEventStub || {});
+
+    this.mapLoaded(currentMock.target);
   }
 
   layout = hbs`
     {{yield (hash
-      on=(component 'mapbox-gl-on' mapboxEventStub=mapboxEventStub)
-      source=(component 'mapbox-gl-source' mapboxEventStub=mapboxEventStub)
+      on=(component 'mapbox-gl-on'
+        map=this
+        mapboxEventStub=mapboxEventStub
+      )
+      source=(component 'mapbox-gl-source'
+        map=this
+        mapboxEventStub=mapboxEventStub
+      )
     )}}
   `;
 }
 
-export class MapboxSourceStub extends Component {}
+export class MapboxSource extends Component {
+}
 
-export class MapboxOnStub extends Component {
+export class MapboxOn extends Component {
   static positionalParams = ['event', 'action'];
 
-  init(...args) {
-    super.init(...args);
+  map;
 
-    this.action(this.mapboxEventStub);
+  bindOnInvocation() {
+    return (() => {
+      const currentMock = deepmerge(defaultMapboxEventStub, this.map.testContext.mapboxEventStub || {});
+
+      return this.action.bind(this, currentMock)();
+    })();
+  }
+
+  init(...args) {
+    this.map.element.addEventListener(this.event, this.bindOnInvocation.bind(this));
+
+    super.init(...args);
   }
 }
 
 export default function(hooks) {
   hooks.beforeEach(function() {
     const that = this;
-    // allow the mouse event callback function signature be injected
-    // via each test run, overriding default no-ops
+    // extend stub and bind in the current test context so it can be
+    // dynamically referenced
     class MapboxGlStub extends MapboxGl {
+      testContext;
+
       init(...args) {
-        this.mapboxEventStub = deepmerge(defaultMapboxEventStub, that.mapboxEventStub || {});
+        this.testContext = that;
 
         super.init(...args);
       }
     }
 
-    this.owner.register('component:mapbox-gl-on', MapboxOnStub);
-    this.owner.register('component:mapbox-gl-source', MapboxSourceStub);
+    this.owner.register('component:mapbox-gl-on', MapboxOn);
+    this.owner.register('component:mapbox-gl-source', MapboxSource);
     this.owner.register('component:mapbox-gl', MapboxGlStub);
   });
 }
