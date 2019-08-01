b565c4b69f2f6cc353d178e609a15157692e234b
trbmcginnis <taylormcgin@gmail.com>
2019-08-01 10:46:58 -0400

    update hearing date

--- app/models/hearing.js
+++ app/models/hearing.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 import { attr, belongsTo } from '@ember-decorators/data';
+import { computed } from '@ember/object';
 
 const { Model } = DS;
 
@@ -9,12 +10,15 @@ export default class HearingModel extends Model {
   // One Hearing to One Project
   @belongsTo('project') project;
 
-  // calculated based on date
-  @attr('boolean') isScheduled;
-
   // sourced from dcp_publichearinglocation
   @attr('string', { defaultValue: '' }) location;
 
   // sourced from dcp_dateofpublichearing
   @attr('date') date;
+
+  // whether hearing is scheduled
+  @computed('date')
+  get isScheduled() {
+    return !!this.get('date');
+  }
 }
