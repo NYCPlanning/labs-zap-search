458ad88c38283de579e6e856e2d532ecebab8644
Matt Gardner <wilburnforce@gmail.com>
2019-09-11 17:54:01 -0400

    Revert deletion of serializer for model keys

---
+++ app/serializers/application.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+import ENV from 'labs-zap-search/config/environment';
+import { dasherize } from '@ember/string';
+
+const { JSONAPISerializer } = DS;
+
+export default class ApplicationSerializer extends JSONAPISerializer {
+  keyForAttribute(key) { // eslint-disable-line
+    return (ENV.environment === 'production' || ENV.environment === 'staging' || ENV.environment === 'devlocal' || ENV.environment === 'devlive') ? key : dasherize(key);
+  }
+}
