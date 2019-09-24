2c9b1ae6b1f288260cc73c6dcd84b04534681249
trbmcginnis <taylormcgin@gmail.com>
2019-09-24 14:49:26 -0400

    660: rec and hearing shortcuts only show when user assigned to project

--- tests/unit/controllers/show-project-test.js
+++ tests/unit/controllers/show-project-test.js
@@ -67,4 +67,62 @@ module('Unit | Controller | show-project', function(hooks) {
 
     assert.equal(hasEmptyFeatures, 0);
   });
+
+  test('isUserAssignedToProject is false when user project ids do not match current project id', async function(assert) {
+    const controller = this.owner.lookup('controller:show-project');
+    assert.ok(controller);
+
+    const user = {
+      id: 1,
+      projects: [
+        {
+          dcp_name: 'P2012M046',
+        },
+        {
+          dcp_name: 'N2014Q176',
+        },
+      ],
+    };
+
+    const project = server.create('project', {
+      dcp_name: 'P2003B056',
+    });
+
+    const projectModel = await this.owner.lookup('service:store').findRecord('project', project.id);
+
+    controller.model = projectModel;
+    controller.user = user;
+    controller.set('session.isAuthenticated', true);
+
+    assert.equal(controller.isUserAssignedToProject, false);
+  });
+
+  test('isUserAssignedToProject is true when user project ids match current project id', async function(assert) {
+    const controller = this.owner.lookup('controller:show-project');
+    assert.ok(controller);
+
+    const user = {
+      id: 1,
+      projects: [
+        {
+          dcp_name: 'N2014Q176',
+        },
+        {
+          dcp_name: 'P2012M046',
+        },
+      ],
+    };
+
+    const project = server.create('project', {
+      dcp_name: 'P2012M046',
+    });
+
+    const projectModel = await this.owner.lookup('service:store').findRecord('project', project.id);
+
+    controller.model = projectModel;
+    controller.user = user;
+    controller.set('session.isAuthenticated', true);
+
+    assert.equal(controller.isUserAssignedToProject, true);
+  });
 });
