02c89a77d23060c1bd9925a431ba2241370bf496
trbmcginnis <taylormcgin@gmail.com>
2019-09-26 11:09:20 -0400

    660: rec and hearing shortcuts only show when user assigned to project (#715)

--- tests/acceptance/authenticated-user-sees-authenticated-features-test.js
+++ tests/acceptance/authenticated-user-sees-authenticated-features-test.js
@@ -24,20 +24,27 @@ module('Acceptance | authenticated user sees authenticated features', function(h
     await invalidateSession();
   });
 
-  hooks.beforeEach(async function() {
-    this.server.create('project', {
-      id: 1,
-    });
-  });
-
   test('User does not see hearing and recommendation buttons on show-project if logged out', async function(assert) {
     await visit('/projects/1');
+
     assert.notOk(find('[data-test-hearing-rec-shortcuts]'));
   });
 
-  test('User sees hearing and recommendation buttons on show-project if logged in', async function(assert) {
+  test('User sees hearing and recommendation buttons on show-project if logged in and project assigned to them', async function(assert) {
+    // user has to be signed in and assigned to that project (dcp_name matches)
+    const userProject = server.create('project', {
+      id: 1,
+      dcp_name: 'P2012M046',
+    });
+
+    this.server.create('project', {
+      id: 1,
+      dcp_name: 'P2012M046',
+    });
+
     this.server.create('user', {
       emailaddress1: 'testuser@planning.nyc.gov',
+      projects: [userProject],
     });
 
     // simulate presence of location hash after OAUTH redirect
@@ -48,5 +55,32 @@ module('Acceptance | authenticated user sees authenticated features', function(h
     await visit('/projects/1');
 
     assert.ok(find('[data-test-hearing-rec-shortcuts]'));
+  });
+
+  test('User does not see hearing and recommendation buttons on show-project if project is not assigned to them', async function(assert) {
+    // user has to be signed in and assigned to that project (dcp_name matches)
+    const userProject = server.create('project', {
+      id: 2,
+      dcp_name: 'N2018Q077',
+    });
+
+    this.server.create('project', {
+      id: 1,
+      dcp_name: 'P2012M046',
+    });
+
+    this.server.create('user', {
+      emailaddress1: 'testuser@planning.nyc.gov',
+      projects: [userProject],
+    });
+
+    // simulate presence of location hash after OAUTH redirect
+    window.location.hash = '#access_token=test';
+
+    await visit('/login');
+
+    await visit('/projects/1');
+
+    assert.notOk(find('[data-test-hearing-rec-shortcuts]'));
   });
 });
