dfcf7f93d07c7cae1cce31bfa123583879831c0e
Godfrey Yeung <godfreyyeung@users.noreply.github.com>
2019-09-26 13:37:34 -0400

    ui validation for dispositionsForAllActions (#716)

---
+++ app/validators/presence-unless-value.js
@@ -0,0 +1,48 @@
+import buildMessage from 'ember-changeset-validations/utils/validation-errors';
+import { validate } from 'ember-validators';
+
+/**
+  * Validator takes one object with the following properties:
+ * @param { bool } presence
+ * @param { String } unless
+ * @param { String } value (optional)
+ * validates a field for presence unless a the `unless` target field has specified `value`
+ */
+export default function validatePresenceUnlessValue(options) {
+  let target;
+  let targetValue;
+
+  if (options && options.unless !== undefined && options.value !== undefined) {
+    if (typeof options.unless === 'string') {
+      target = options.unless;
+    }
+    if (typeof options.value === 'string') {
+      targetValue = options.value;
+    }
+
+    delete options.unless;
+    delete options.value;
+  }
+
+  return (key, value, _oldValue, changes, content) => {
+    if (target && (changes[target] || (changes[target] === undefined && content[target]))) {
+      if ((changes[target] === targetValue) || (changes[target] === undefined && (content[target] === targetValue))) {
+        return true;
+      }
+    }
+
+    const result = validate('presence', value, options, null, key);
+
+    if (typeof result === 'boolean' || typeof result === 'string') {
+      return result;
+    }
+    // We flipped the meaning behind `present` and `blank` so switch the two
+    if (result.type === 'present') {
+      result.type = 'blank';
+    } else if (result.type === 'blank') {
+      result.type = 'present';
+    }
+
+    return buildMessage(key, result);
+  };
+}
