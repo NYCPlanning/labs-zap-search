7b5378e3725d924cba9198dfa49490fe5143e057
Godfrey Yeung <godfreyyeung@users.noreply.github.com>
2019-10-02 11:32:50 -0400

    New Project attributes and real CB milestone mirage data (#727)

--- app/models/project.js
+++ app/models/project.js
@@ -16,6 +16,16 @@ const EmptyFeatureCollection = {
   }],
 };
 
+const COMMUNITY_BOARD_REFERRAL_MILESTONE_ID = '923beec4-dad0-e711-8116-1458d04e2fb8';
+const BOROUGH_PRESIDENT_REFERRAL_MILESTONE_ID = '943beec4-dad0-e711-8116-1458d04e2fb8';
+const BOROUGH_BOARD_REFERRAL_MILESTONE_ID = '963beec4-dad0-e711-8116-1458d04e2fb8';
+
+const MILESTONE_ID_LOOKUP = {
+  CB: COMMUNITY_BOARD_REFERRAL_MILESTONE_ID,
+  BP: BOROUGH_PRESIDENT_REFERRAL_MILESTONE_ID,
+  BB: BOROUGH_BOARD_REFERRAL_MILESTONE_ID,
+};
+
 export default class ProjectModel extends Model {
   // Many Users to Many Projects
   @hasMany('user') users;
@@ -31,7 +41,86 @@ export default class ProjectModel extends Model {
 
   @hasMany('milestone', { async: false }) milestones;
 
+  // Attributes for dashboard view
+  @attr() tab;
+
+  @attr() teammemberrole;
+
   @attr() applicantteam;
+
+  @computed('milestones')
+  get publicReviewPlannedStartDate() {
+    const { dcpPlannedstartdate } = this.milestones.find(milestone => milestone.dcpMilestone === COMMUNITY_BOARD_REFERRAL_MILESTONE_ID) || {};
+    return dcpPlannedstartdate || null;
+  }
+
+  // The following <tab>MilestoneActual<Start/End>
+  // date fields represent different dates from different milestones
+  // depending on `tab`:
+
+  // If `tab` is `upcoming`...
+  // if the project is in public review, this field is used to display the participant's review planned start date.
+  // else this field returns null
+  @computed('tab', 'teammemberrole', 'milestones')
+  get upcomingMilestonePlannedStartDate() {
+    if (this.tab === 'upcoming') {
+      if (this.dcpPublicstatusSimp !== 'filed') {
+        const participantMilestoneId = MILESTONE_ID_LOOKUP[this.teammemberrole];
+        const participantReviewMilestone = this.milestones.find(milestone => milestone.dcpMilestone === participantMilestoneId);
+        return participantReviewMilestone ? participantReviewMilestone.dcpPlannedstartdate : null;
+      }
+    }
+    return null;
+  }
+
+  // If `tab` is to-review', these start/end dates are derived from
+  // the participant's (specified by `teammemberrole`) review milestone.
+  @computed('tab', 'teammemberrole', 'milestones')
+  get toReviewMilestoneActualStartDate() {
+    if (this.tab !== 'to-review') {
+      return null;
+    }
+    const participantMilestoneId = MILESTONE_ID_LOOKUP[this.teammemberrole];
+    const { dcpActualstartdate } = this.milestones.find(milestone => milestone.dcpMilestone === participantMilestoneId) || {};
+    return dcpActualstartdate;
+  }
+
+  @computed('tab', 'teammemberrole', 'milestones')
+  get toReviewMilestoneActualEndDate() {
+    if (this.tab !== 'to-review') {
+      return null;
+    }
+    const participantMilestoneId = MILESTONE_ID_LOOKUP[this.teammemberrole];
+    const { dcpActualenddate } = this.milestones.find(milestone => milestone.dcpMilestone === participantMilestoneId) || {};
+    return dcpActualenddate;
+  }
+
+  // If `tab` is 'reviewed'...
+  //   - these start/end dates come from the current In Progress milestone
+  //   - an array of milestone dates is returned
+  @computed('tab', 'teammemberrole', 'milestones')
+  get reviewedMilestoneActualStartDate() {
+    if (this.tab !== 'reviewed') {
+      return null;
+    }
+    const inProgressMilestones = this.milestones.filter(milestone => milestone.statuscode === 'In Progress');
+    return inProgressMilestones.map(milestone => ({
+      milestone: milestone.dcpMilestone,
+      dcpActualstartdate: milestone.dcpActualstartdate,
+    }));
+  }
+
+  @computed('tab', 'teammemberrole', 'milestones')
+  get reviewedMilestoneActualEndDate() {
+    if (this.tab !== 'reviewed') {
+      return null;
+    }
+    const inProgressMilestones = this.milestones.filter(milestone => milestone.statuscode === 'In Progress');
+    return inProgressMilestones.map(milestone => ({
+      milestone: milestone.dcpMilestone,
+      dcpActualenddate: milestone.dcpActualenddate,
+    }));
+  }
 
   // array of applicant objects
   @attr() applicants;
