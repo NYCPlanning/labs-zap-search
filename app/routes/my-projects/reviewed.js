d367da90bc3eb3798e224fdf2ac3dce68a764a96
Godfrey Yeung <godfreyyeung@users.noreply.github.com>
2019-10-03 11:01:40 -0400

    display milestone days remaining and duration on project cards (#734)

--- app/routes/my-projects/reviewed.js
+++ app/routes/my-projects/reviewed.js
@@ -8,13 +8,11 @@ export default class MyProjectsReviewedRoute extends Route {
   @service
   store;
 
-  // This route loads the user's projects according to their milestones and associated userProjectParticipantTypes.
-  // Specifically, a given user's project only shows up here if...
-  // 1) it has an "Review" (a.k.a. "Referral") milestone that has a statuscode of "Not Started"
-  // 2) it matches one of the project's -- and the current user's -- associated userProjectParticipantTypes
-  // For example, for a Borough President user, a project shows up here if it has an "Reviewed" Borough President Referral milestone.
   async model() {
-    // Use this endpoint for now. This will need to be updated when the backend is finalized.
-    return this.store.query('project', { project_lup_status: 'reviewed' });
+    const reviewedProjects = await this.store.query('project', { project_lup_status: 'reviewed', include: 'actions,milestones,dispositions.action' }, {
+      reload: true,
+    });
+
+    return reviewedProjects;
   }
 }
