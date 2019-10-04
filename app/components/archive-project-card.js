16ae96ed6efd4f8f8e841eb53c19522e59c26ea7
godfreyyeung <godfreyyeung@gmail.com>
2019-10-04 11:45:16 -0400

    filtered milestones with recommendation outcome icons on archive projects

---
+++ app/components/archive-project-card.js
@@ -0,0 +1,9 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+
+export default class ReviewedProjectCardComponent extends Component {
+  @service
+  milestoneConstants;
+
+  project = {};
+}
--- tests/integration/components/archive-project-card-test.js
+++ tests/integration/components/archive-project-card-test.js
@@ -3,13 +3,13 @@ import { setupRenderingTest } from 'ember-qunit';
 import { find, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | reviewed-project-card', function(hooks) {
+module('Integration | Component | archive-project-card', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    this.set('project', { reviewedMilestoneActualStartEndDates: [] });
+    this.set('project', {});
 
-    await render(hbs`<ReviewedProjectCard @project={{this.project}} />`);
+    await render(hbs`<ArchiveProjectCard @project={{this.project}} />`);
 
     assert.ok(find('[data-test-project-card]'));
   });
