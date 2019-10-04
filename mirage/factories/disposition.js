8a8fc895420175ba42480c961d72ff6b22a5a2d0
Godfrey Yeung <godfreyyeung@users.noreply.github.com>
2019-10-04 11:23:53 -0400

    display disposition recommendations as icons on reviewed projects (#756)

--- mirage/factories/disposition.js
+++ mirage/factories/disposition.js
@@ -1,6 +1,6 @@
-import { Factory, faker } from 'ember-cli-mirage';
+import { Factory, faker, trait } from 'ember-cli-mirage';
 
-export default class DispositionFactory extends Factory {
+export default Factory.extend({
   // #### Recommendation Type per Each of the 3 Participants ####
   // sourced from dcp_boroughpresidentrecommendation
   // e.g. 'Favorable', 'Conditional Favorable', 'Unfavorable', 'Conditional Unfavorable',
@@ -14,63 +14,81 @@ export default class DispositionFactory extends Factory {
   // 'Non-Complying', 'Vote Quorum Not Present', 'Received after Clock Expired', 'No Objection', 'Waiver of Recommendation',
   // N/A as default
 
-  boroughpresidentrecommendation() {
-    return faker.random.arrayElement(['Approved', 'Approved with Modifications/Conditions', 'Disapproved', 'Disapprovd with Modifications/Conditions', 'Waived']);
-  }
-
-  boroughboardrecommendation() {
-    return faker.random.arrayElement(['Approved', 'Approved with Modifications/Conditions', 'Disapproved', 'Disapprovd with Modifications/Conditions', 'Waived']);
-  }
-
-  communityboardrecommendation() {
-    return faker.random.arrayElement(['Approved', 'Approved with Modifications/Conditions', 'Disapproved', 'Disapprovd with Modifications/Conditions', 'Waived']);
-  }
-
   consideration() {
-    return faker.Lorem.sentences();
-  }
+    return faker.lorem.sentences();
+  },
 
   votelocation() {
-    return faker.Address.streetAddress();
-  }
+    return faker.address.streetAddress();
+  },
 
   datereceived() {
-    return faker.Date.past();
-  }
+    return faker.date.past();
+  },
 
   dateofvote() {
-    return faker.Date.past();
-  }
+    return faker.date.past();
+  },
 
   votinginfavorrecommendation() {
     return 15;
-  }
+  },
 
   votingagainstrecommendation() {
     return 4;
-  }
+  },
 
   votingabstainingonrecommendation() {
     return 1;
-  }
+  },
 
   totalmembersappointedtotheboard() {
     return 20;
-  }
+  },
 
   wasaquorumpresent() {
     return null;
-  }
+  },
 
   statecode(i) {
     return faker.list.random('Active', 'Inactive')(i);
-  }
+  },
 
   statuscode(i) {
     return faker.list.random('Draft', 'Saved', 'Submitted', 'Deactivated', 'Not Submitted')(i);
-  }
+  },
 
   docketdescription() {
-    return faker.Lorem.sentence();
-  }
-}
+    return faker.lorem.sentence();
+  },
+
+  communityBoardDisposition: trait({
+    boroughpresidentrecommendation: null,
+
+    boroughboardrecommendation: null,
+
+    communityboardrecommendation() {
+      return faker.random.arrayElement(['Approved', 'Approved with Modifications/Conditions', 'Disapproved', 'Disapproved with Modifications/Conditions', 'Waived']);
+    },
+  }),
+
+  boroughBoardDisposition: trait({
+    boroughpresidentrecommendation: null,
+
+    boroughboardrecommendation() {
+      return faker.random.arrayElement(['Approved', 'Approved with Modifications/Conditions', 'Disapproved', 'Disapproved with Modifications/Conditions', 'Waived']);
+    },
+
+    communityboardrecommendation: null,
+  }),
+
+  boroughPresidentDisposition: trait({
+    boroughpresidentrecommendation() {
+      return faker.random.arrayElement(['Approved', 'Approved with Modifications/Conditions', 'Disapproved', 'Disapproved with Modifications/Conditions', 'Waived']);
+    },
+
+    boroughboardrecommendation: null,
+
+    communityboardrecommendation: null,
+  }),
+});
