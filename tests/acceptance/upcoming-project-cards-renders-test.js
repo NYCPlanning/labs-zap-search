e0ef2d8c16740fca204c06d1144732e5c594ff88
Godfrey Yeung <godfreyyeung@users.noreply.github.com>
2019-10-23 19:55:20 -0400

    patch condition for displaying fuzzy public review start date on upcoming project card (#835)

--- tests/acceptance/upcoming-project-cards-renders-test.js
+++ tests/acceptance/upcoming-project-cards-renders-test.js
@@ -33,13 +33,15 @@ module('Acceptance | upcoming project cards renders', function(hooks) {
         this.server.create('assignment', {
           id: 1,
           tab: 'upcoming',
-          dcpPublicstatus: 'Filed',
           dcpLupteammemberrole: 'BP',
           dispositions: [],
           project: this.server.create('project', {
-            milestones: [this.server.create('milestone', 'communityBoardReview', {
-              dcpPlannedstartdate: moment().add(32, 'days'),
-            })],
+            dcpPublicstatusSimp: 'Filed',
+            milestones: [
+              this.server.create('milestone', 'communityBoardReview', {
+                dcpPlannedstartdate: moment().add(32, 'days'),
+              }),
+            ],
           }),
         }),
       ],
@@ -56,7 +58,7 @@ module('Acceptance | upcoming project cards renders', function(hooks) {
     assert.notOk(find('[data-test-time-remaining]'));
   });
 
-  test('upcoming project card renders fuzzy date if public review is less than 30 days away', async function(assert) {
+  test('upcoming project card renders fuzzy date if public review is under 30 days away', async function(assert) {
     this.server.create('user', {
       id: 1,
       email: 'qncb5@planning.nyc.gov',
@@ -65,10 +67,10 @@ module('Acceptance | upcoming project cards renders', function(hooks) {
         this.server.create('assignment', {
           id: 1,
           tab: 'upcoming',
-          dcpPublicstatus: 'Filed',
           dcpLupteammemberrole: 'BP',
           dispositions: [],
           project: this.server.create('project', {
+            dcpPublicstatusSimp: 'Filed',
             milestones: [this.server.create('milestone', 'communityBoardReview', {
               dcpPlannedstartdate: moment().add(15, 'days'),
             })],
@@ -84,7 +86,7 @@ module('Acceptance | upcoming project cards renders', function(hooks) {
     await visit('/my-projects/upcoming');
 
     const timeRemainingValue = find('[data-test-fuzzy-time-remaining]').textContent.trim();
-    assert.equal(timeRemainingValue, '< 30 days', 'Time remaining displays less than 30 days');
+    assert.equal(timeRemainingValue, 'under 30 days', 'Time remaining displays under 30 days');
     assert.notOk(find('[data-test-time-remaining]'));
   });
 
@@ -97,10 +99,10 @@ module('Acceptance | upcoming project cards renders', function(hooks) {
         this.server.create('assignment', {
           id: 1,
           tab: 'upcoming',
-          dcpPublicstatus: 'Certified',
           dcpLupteammemberrole: 'BP',
           dispositions: [],
           project: this.server.create('project', {
+            dcpPublicstatusSimp: 'In Public Review',
             milestones: [
               this.server.create('milestone', 'communityBoardReview', {
               }),
@@ -122,5 +124,39 @@ module('Acceptance | upcoming project cards renders', function(hooks) {
     const timeRemainingValue = find('[data-test-time-remaining]').textContent.trim();
     assert.equal(timeRemainingValue, '11/30/2019', 'Estimated start is 11/30/2019');
     assert.notOk(find('[data-test-fuzzy-time-remaining]'));
+  });
+
+  test('upcoming project card defaults to fuzzy date if project.dcpPublicstatusSimp is undefined', async function(assert) {
+    this.server.create('user', {
+      id: 1,
+      email: 'qncb5@planning.nyc.gov',
+      landUseParticipant: 'QNBP',
+      assignments: [
+        this.server.create('assignment', {
+          id: 1,
+          tab: 'upcoming',
+          dcpLupteammemberrole: 'BP',
+          dispositions: [],
+          project: this.server.create('project', {
+            dcpPublicstatusSimp: '',
+            milestones: [this.server.create('milestone', 'communityBoardReview', {
+              // note how Community Board Review has already started, but we still display
+              // the fuzzy public review start date ('under 30 days')
+              dcpPlannedstartdate: moment().subtract(15, 'days'),
+            })],
+          }),
+        }),
+      ],
+    });
+
+    await authenticateSession({
+      id: 1,
+    });
+
+    await visit('/my-projects/upcoming');
+
+    const timeRemainingValue = find('[data-test-fuzzy-time-remaining]').textContent.trim();
+    assert.equal(timeRemainingValue, 'under 30 days', 'Time remaining displays under 30 days');
+    assert.notOk(find('[data-test-time-remaining]'));
   });
 });
