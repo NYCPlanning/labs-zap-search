6d7d661b7472a730504b4c6ede71dab913acd958
Matt Gardner <wilburnforce@gmail.com>
2019-10-23 12:25:39 -0400

    Add more file upload functionality (#829)
    * Add more file upload functionality
    
    Unclear at this point how we want to handle uploads
    
    * Change action name
    
    * improve layout of rec form file upload
    
    * allow multiple files to be selected for upload

---
+++ app/components/recommendation-upload-file.js
@@ -0,0 +1,9 @@
+import Component from '@ember/component';
+import { action } from '@ember/object';
+
+export default class RecommendationUploadFile extends Component {
+  @action
+  uploadFile() {
+    // TODO: handle uploaded file!
+  }
+}
