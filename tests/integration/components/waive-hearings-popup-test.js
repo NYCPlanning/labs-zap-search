f08d8454232ac5fa3c76e31db4a56257b15da11d
trbmcginnis <taylormcgin@gmail.com>
2019-11-14 18:22:49 -0500

    900: fix waive hearings by adding .save()

--- tests/integration/components/waive-hearings-popup-test.js
+++ tests/integration/components/waive-hearings-popup-test.js
@@ -2,8 +2,6 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import { updateEachObjectInArray } from 'labs-zap-search/components/waive-hearings-popup';
-import EmberObject from '@ember/object';
 
 module('Integration | Component | waive-hearings-popup', function(hooks) {
   setupRenderingTest(hooks);
@@ -25,43 +23,5 @@ module('Integration | Component | waive-hearings-popup', function(hooks) {
     const messageVisible = waiveHearingsMessageOnPopup.includes('Are you sure you want to opt out of noticing a public hearing?');
 
     assert.ok(messageVisible);
-  });
-
-  test('updateEachObjectInArray function works', function(assert) {
-    const ourDisps = EmberObject.extend({});
-
-    const disp1 = ourDisps.create({
-      id: 1,
-      dcpPublichearinglocation: '',
-      dcpDateofpublichearing: null,
-    });
-
-    const disp2 = ourDisps.create({
-      id: 2,
-      dcpPublichearinglocation: '',
-      dcpDateofpublichearing: null,
-    });
-
-    const disp3 = ourDisps.create({
-      id: 3,
-      dcpPublichearinglocation: '',
-      dcpDateofpublichearing: null,
-    });
-
-    const disp4 = ourDisps.create({
-      id: 4,
-      dcpPublichearinglocation: '',
-      dcpDateofpublichearing: null,
-    });
-
-    // mimics each project's array of dispositions
-    const dispositions = [disp1, disp2, disp3, disp4];
-
-    // run the updateEachObjectInArray function, which...
-    updateEachObjectInArray(dispositions, 'dcpPublichearinglocation', 'waived');
-
-    const eachLocation = dispositions.map(disp => disp.dcpPublichearinglocation);
-
-    assert.equal(eachLocation.join(','), 'waived,waived,waived,waived');
   });
 });
