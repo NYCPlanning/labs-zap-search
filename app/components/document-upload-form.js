a4101eb59e1964165141094a2a3b7f4b6bbb0884
Andy Cochran <acochran@planning.nyc.gov>
2019-11-21 17:53:28 -0500

    add UI to remove files from up load queue

--- app/components/document-upload-form.js
+++ app/components/document-upload-form.js
@@ -1,8 +1,15 @@
 import Component from '@ember/component';
+import { action } from '@ember/object';
+import Queue from 'ember-file-upload/queue';
 
 export default class DocumentUploadForm extends Component {
   // name of the queue
   queueName = 'application'
 
   onFileAdd = () => {};
+
+  @action
+  removeFile(queue, file) {
+    queue.remove(file);
+  }
 }
