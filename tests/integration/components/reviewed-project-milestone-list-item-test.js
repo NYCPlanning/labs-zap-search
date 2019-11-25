b52cbd0a9ff3de0270e0b9522fbf6077763a317c
trbmcginnis <taylormcgin@gmail.com>
2019-11-25 14:12:41 -0500

    1002: use dcpActualenddate instead of dcpDatereceived for review milestones in milestones list

--- tests/integration/components/reviewed-project-milestone-list-item-test.js
+++ tests/integration/components/reviewed-project-milestone-list-item-test.js
@@ -1,73 +1,15 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { find, render } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import EmberObject from '@ember/object';
 
 module('Integration | Component | reviewed-project-milestone-list-item', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it does not display "Recommendation Submitted" when there are no dispos', async function(assert) {
-    this.set('project', { dispositions: [] });
-
-    this.set('milestone', {});
-
-    await render(hbs`<ReviewedProjectMilestoneListItem @project={{this.project}} @milestone={{this.milestone}}/>`);
-
-    assert.notOk(find('[data-test-cb-disposition-date-received]'));
-  });
-
-
-  test('it does not display "Recommendation Submitted" date when there is no recommendation', async function(assert) {
-    this.set(
-      'project',
-      {
-        dispositions: [
-          EmberObject.create({
-            dcpCommunityboardrecommendation: '',
-            dcpDatereceived: null,
-          }),
-        ],
-      },
-    );
-
-    this.set('milestone', {});
-
-    await render(hbs`<ReviewedProjectMilestoneListItem @project={{this.project}} @milestone={{this.milestone}}/>`);
-
-    assert.notOk(find('[data-test-cb-disposition-date-received]'));
-  });
-
-
-  test('it does not display "Recommendation Submitted" date when there is a recommendation but there is no date received', async function(assert) {
-    this.set(
-      'project',
-      {
-        dispositions: [
-          EmberObject.create({
-            dcpCommunityboardrecommendation: 'foo',
-            dcpDatereceived: null,
-          }),
-        ],
-      },
-    );
-
-    this.set('milestone', {});
-
-    await render(hbs`<ReviewedProjectMilestoneListItem @project={{this.project}} @milestone={{this.milestone}}/>`);
-
-    assert.notOk(find('[data-test-cb-disposition-date-received]'));
-  });
-
-
   test('it displays a Community Board recommendation date', async function(assert) {
     this.set('project', {
-      dispositions: [
-        EmberObject.create({
-          dcpCommunityboardrecommendation: 'foo',
-          dcpDatereceived: new Date(),
-        }),
-      ],
+      dispositions: [],
     });
 
     this.set(
@@ -82,7 +24,7 @@ module('Integration | Component | reviewed-project-milestone-list-item', functio
         displayDate2: null,
         displayDate: '2019-10-05T20:31:57.956Z',
         statuscode: 'Completed',
-        dcpActualenddate: null,
+        dcpActualenddate: new Date('2020-12-15T00:00:00'),
         dcpActualstartdate: null,
         dcpPlannedcompletiondate: null,
         dcpPlannedstartdate: '2019-10-05T20:31:57.956Z',
@@ -92,6 +34,7 @@ module('Integration | Component | reviewed-project-milestone-list-item', functio
 
     await render(hbs`<ReviewedProjectMilestoneListItem @project={{this.project}} @milestone={{this.milestone}}/>`);
 
-    assert.ok(find('[data-test-cb-disposition-date-received]'));
+    assert.ok(this.element.textContent.includes('Community Board Review'));
+    assert.ok(this.element.textContent.includes('12/15/2020'));
   });
 });
