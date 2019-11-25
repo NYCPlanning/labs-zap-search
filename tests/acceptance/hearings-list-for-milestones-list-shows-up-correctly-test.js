1f50e58b560dd6388958b07be9674328775c0b7a
trbmcginnis <taylormcgin@gmail.com>
2019-11-25 14:44:14 -0500

    1007: remove sub-milestones on reviewed and archive tabs (#1014)

--- tests/acceptance/hearings-list-for-milestones-list-shows-up-correctly-test.js
+++ tests/acceptance/hearings-list-for-milestones-list-shows-up-correctly-test.js
@@ -341,322 +341,99 @@ module('Acceptance | hearings list for milestones list shows up correctly', func
   //
   });
 
-  test('hearings-list-for-milestones-list shows up on reviewed tab', async function(assert) {
-    // ########## Reviewed #################################################################
-    this.server.create('assignment', {
+  test('milestones list is not broken by a NULL fullname value on show-project page', async function(assert) {
+    this.server.create('project', {
       id: 5,
-      tab: 'reviewed',
-      project: this.server.create('project', {
-        dispositions: [
-          server.create('disposition', {
-            id: 17,
-            dcpPublichearinglocation: 'Purple Street',
-            dcpDateofpublichearing: new Date('2020-10-21T01:30:00'),
-            fullname: 'QN BB',
-            dcpProjectaction: '1',
-            // action: server.create('action', { dcpName: 'Zoning Special Permit', dcpUlurpnumber: 'C780076TLK' }),
-          }),
-          server.create('disposition', {
-            id: 18,
-            dcpPublichearinglocation: 'Green Street',
-            dcpDateofpublichearing: new Date('2020-04-21T01:30:00'),
-            fullname: 'MN BB',
-            dcpProjectaction: '2',
-            // action: server.create('action', { dcpName: 'Zoning Text Amendment', dcpUlurpnumber: 'N860877TCM' }),
-          }),
-        ],
-        actions: [
-          server.create('action', { id: '1', dcpName: 'Zoning Special Permit', dcpUlurpnumber: 'C780076TLK' }),
-          server.create('action', { id: '2', dcpName: 'Zoning Text Amendment', dcpUlurpnumber: 'N860877TCM' }),
-        ],
-        milestones: [
-          server.create('milestone', {
-            displayName: 'Land Use Application Filed',
-            dcpMilestonesequence: 26,
-            milestonename: 'Land Use Application Filed',
-            dcpMilestone: '663beec4-dad0-e711-8116-1458d04e2fb8',
-            dcpMilestoneoutcome: null,
-            displayDate2: null,
-            displayDate: '2019-07-13T19:31:57.763Z',
-            statuscode: 'Completed',
-            dcpActualenddate: null,
-            dcpActualstartdate: '2019-07-13T19:31:57.763Z',
-            dcpPlannedcompletiondate: null,
-            dcpPlannedstartdate: null,
-            id: '1',
-          }),
-          server.create('milestone', {
-            displayName: 'Application Reviewed at City Planning Commission Review Session',
-            dcpMilestonesequence: 46,
-            milestonename: 'Application Reviewed at City Planning Commission Review Session',
-            dcpMilestone: '8e3beec4-dad0-e711-8116-1458d04e2fb8',
-            milestoneLinks: [],
-            dcpMilestoneoutcome: null,
-            displayDate2: null,
-            displayDate: '2019-10-18T19:31:57.916Z',
-            statuscode: 'Not Started',
-            dcpActualenddate: null,
-            dcpActualstartdate: '2019-10-18T19:31:57.916Z',
-            dcpPlannedcompletiondate: null,
-            dcpPlannedstartdate: null,
-            id: '11',
-          }),
-          server.create('milestone', {
-            displayName: 'Community Board Review',
-            dcpMilestonesequence: 48,
-            milestonename: 'Community Board Review',
-            dcpMilestone: '923beec4-dad0-e711-8116-1458d04e2fb8',
-            milestoneLinks: [],
-            dcpMilestoneoutcome: null,
-            displayDate2: null,
-            displayDate: '2019-11-17T20:31:57.956Z',
-            statuscode: 'Not Started',
-            dcpActualenddate: null,
-            dcpActualstartdate: null,
-            dcpPlannedcompletiondate: null,
-            dcpPlannedstartdate: '2019-11-17T20:31:57.956Z',
-            id: '12',
-          }),
-          server.create('milestone', {
-            displayName: 'Borough Board Review',
-            dcpMilestonesequence: 50,
-            milestonename: 'Borough Board Review',
-            dcpMilestone: '963beec4-dad0-e711-8116-1458d04e2fb8',
-            milestoneLinks: [],
-            dcpMilestoneoutcome: null,
-            displayDate2: '2019-11-06T20:31:58.519Z',
-            displayDate: '2019-10-07T19:31:58.519Z',
-            statuscode: 'In Progress',
-            dcpActualenddate: '2019-11-06T20:31:58.519Z',
-            dcpActualstartdate: '2019-10-07T19:31:58.519Z',
-            dcpPlannedcompletiondate: null,
-            dcpPlannedstartdate: null,
-            id: '38',
-          }),
-        ],
-      }),
-    });
-
-    await visit('/my-projects/reviewed');
-
-    assert.ok(this.element.querySelector('[data-test-lup-full-name="Queens Borough Board"]').textContent.includes('Queens Borough Board Public Hearing'));
-    assert.ok(this.element.querySelector('[data-test-lup-full-name="Manhattan Borough Board"]').textContent.includes('Manhattan Borough Board Public Hearing'));
-    assert.ok(this.element.querySelector('[data-test-hearing-actions-list="170"]').textContent.includes('Zoning Special Permit'), 'action 170');
-    assert.ok(this.element.querySelector('[data-test-hearing-actions-list="180"]').textContent.includes('Zoning Text Amendment'), 'action 180');
-    assert.ok(this.element.querySelector('[data-test-hearing-location="17"]').textContent.includes('Purple Street'));
-    assert.ok(this.element.querySelector('[data-test-hearing-date="17"]').textContent.includes('October 21'));
-    assert.ok(this.element.querySelector('[data-test-hearing-location="18"]').textContent.includes('Green Street'));
-    assert.ok(this.element.querySelector('[data-test-hearing-date="18"]').textContent.includes('April 21'));
-  });
-
-  test('hearings-list-for-milestones-list shows up on archive tab', async function(assert) {
-    // ########## Archive #################################################################
-    this.server.create('assignment', {
-      id: 5,
-      tab: 'archive',
-      project: this.server.create('project', {
-        dispositions: [
-          server.create('disposition', {
-            id: 17,
-            dcpPublichearinglocation: 'Purple Street',
-            dcpDateofpublichearing: new Date('2020-10-21T01:30:00'),
-            fullname: 'QN BB',
-            dcpProjectaction: '1',
-            // action: server.create('action', { dcpName: 'Zoning Special Permit', dcpUlurpnumber: 'C780076TLK' }),
-          }),
-          server.create('disposition', {
-            id: 18,
-            dcpPublichearinglocation: 'Green Street',
-            dcpDateofpublichearing: new Date('2020-04-21T01:30:00'),
-            fullname: 'MN BB',
-            dcpProjectaction: '2',
-            // action: server.create('action', { dcpName: 'Zoning Text Amendment', dcpUlurpnumber: 'N860877TCM' }),
-          }),
-        ],
-        actions: [
-          server.create('action', { id: '1', dcpName: 'Zoning Special Permit', dcpUlurpnumber: 'C780076TLK' }),
-          server.create('action', { id: '2', dcpName: 'Zoning Text Amendment', dcpUlurpnumber: 'N860877TCM' }),
-        ],
-        milestones: [
-          server.create('milestone', {
-            displayName: 'Land Use Application Filed',
-            dcpMilestonesequence: 26,
-            milestonename: 'Land Use Application Filed',
-            dcpMilestone: '663beec4-dad0-e711-8116-1458d04e2fb8',
-            dcpMilestoneoutcome: null,
-            displayDate2: null,
-            displayDate: '2019-07-13T19:31:57.763Z',
-            statuscode: 'Completed',
-            dcpActualenddate: null,
-            dcpActualstartdate: '2019-07-13T19:31:57.763Z',
-            dcpPlannedcompletiondate: null,
-            dcpPlannedstartdate: null,
-            id: '1',
-          }),
-          server.create('milestone', {
-            displayName: 'Application Reviewed at City Planning Commission Review Session',
-            dcpMilestonesequence: 46,
-            milestonename: 'Application Reviewed at City Planning Commission Review Session',
-            dcpMilestone: '8e3beec4-dad0-e711-8116-1458d04e2fb8',
-            milestoneLinks: [],
-            dcpMilestoneoutcome: null,
-            displayDate2: null,
-            displayDate: '2019-10-18T19:31:57.916Z',
-            statuscode: 'Not Started',
-            dcpActualenddate: null,
-            dcpActualstartdate: '2019-10-18T19:31:57.916Z',
-            dcpPlannedcompletiondate: null,
-            dcpPlannedstartdate: null,
-            id: '11',
-          }),
-          server.create('milestone', {
-            displayName: 'Community Board Review',
-            dcpMilestonesequence: 48,
-            milestonename: 'Community Board Review',
-            dcpMilestone: '923beec4-dad0-e711-8116-1458d04e2fb8',
-            milestoneLinks: [],
-            dcpMilestoneoutcome: null,
-            displayDate2: null,
-            displayDate: '2019-11-17T20:31:57.956Z',
-            statuscode: 'Not Started',
-            dcpActualenddate: null,
-            dcpActualstartdate: null,
-            dcpPlannedcompletiondate: null,
-            dcpPlannedstartdate: '2019-11-17T20:31:57.956Z',
-            id: '12',
-          }),
-          server.create('milestone', {
-            displayName: 'Borough Board Review',
-            dcpMilestonesequence: 50,
-            milestonename: 'Borough Board Review',
-            dcpMilestone: '963beec4-dad0-e711-8116-1458d04e2fb8',
-            milestoneLinks: [],
-            dcpMilestoneoutcome: null,
-            displayDate2: '2019-11-06T20:31:58.519Z',
-            displayDate: '2019-10-07T19:31:58.519Z',
-            statuscode: 'In Progress',
-            dcpActualenddate: '2019-11-06T20:31:58.519Z',
-            dcpActualstartdate: '2019-10-07T19:31:58.519Z',
-            dcpPlannedcompletiondate: null,
-            dcpPlannedstartdate: null,
-            id: '38',
-          }),
-        ],
-      }),
-    });
-
-    await visit('/my-projects/archive');
-
-    assert.ok(this.element.querySelector('[data-test-lup-full-name="Queens Borough Board"]').textContent.includes('Queens Borough Board Public Hearing'));
-    assert.ok(this.element.querySelector('[data-test-lup-full-name="Manhattan Borough Board"]').textContent.includes('Manhattan Borough Board Public Hearing'));
-    assert.ok(this.element.querySelector('[data-test-hearing-actions-list="170"]').textContent.includes('Zoning Special Permit'), 'action 170');
-    assert.ok(this.element.querySelector('[data-test-hearing-actions-list="180"]').textContent.includes('Zoning Text Amendment'), 'action 180');
-    assert.ok(this.element.querySelector('[data-test-hearing-location="17"]').textContent.includes('Purple Street'));
-    assert.ok(this.element.querySelector('[data-test-hearing-date="17"]').textContent.includes('October 21'));
-    assert.ok(this.element.querySelector('[data-test-hearing-location="18"]').textContent.includes('Green Street'));
-    assert.ok(this.element.querySelector('[data-test-hearing-date="18"]').textContent.includes('April 21'));
-  });
-
-  test('hearings-list-for-milestones-list does not break when NULL value for fullname on archive tab', async function(assert) {
-    this.server.create('assignment', {
-      id: 5,
-      tab: 'archive',
       dispositions: [
         server.create('disposition', {
           id: 17,
-          dcpPublichearinglocation: 'Purple Street',
+          dcpPublichearinglocation: '121 Bananas Avenue, Queens',
           dcpDateofpublichearing: new Date('2020-10-21T01:30:00'),
           fullname: null,
           dcpProjectaction: '1',
           // action: server.create('action', { dcpName: 'Zoning Special Permit', dcpUlurpnumber: 'C780076TLK' }),
         }),
       ],
-      project: this.server.create('project', {
-        dispositions: [
-          server.create('disposition', {
-            id: 17,
-            dcpPublichearinglocation: 'Purple Street',
-            dcpDateofpublichearing: new Date('2020-10-21T01:30:00'),
-            fullname: null,
-            dcpProjectaction: '1',
-            // action: server.create('action', { dcpName: 'Zoning Special Permit', dcpUlurpnumber: 'C780076TLK' }),
-          }),
-        ],
-        actions: [
-          server.create('action', { id: '1', dcpName: 'Zoning Special Permit', dcpUlurpnumber: 'C780076TLK' }),
-        ],
-        milestones: [
-          server.create('milestone', {
-            displayName: 'Land Use Application Filed',
-            dcpMilestonesequence: 26,
-            milestonename: 'Land Use Application Filed',
-            dcpMilestone: '663beec4-dad0-e711-8116-1458d04e2fb8',
-            dcpMilestoneoutcome: null,
-            displayDate2: null,
-            displayDate: '2019-07-13T19:31:57.763Z',
-            statuscode: 'Completed',
-            dcpActualenddate: null,
-            dcpActualstartdate: '2019-07-13T19:31:57.763Z',
-            dcpPlannedcompletiondate: null,
-            dcpPlannedstartdate: null,
-            id: '1',
-          }),
-          server.create('milestone', {
-            displayName: 'Application Reviewed at City Planning Commission Review Session',
-            dcpMilestonesequence: 46,
-            milestonename: 'Application Reviewed at City Planning Commission Review Session',
-            dcpMilestone: '8e3beec4-dad0-e711-8116-1458d04e2fb8',
-            milestoneLinks: [],
-            dcpMilestoneoutcome: null,
-            displayDate2: null,
-            displayDate: '2019-10-18T19:31:57.916Z',
-            statuscode: 'Not Started',
-            dcpActualenddate: null,
-            dcpActualstartdate: '2019-10-18T19:31:57.916Z',
-            dcpPlannedcompletiondate: null,
-            dcpPlannedstartdate: null,
-            id: '11',
-          }),
-          server.create('milestone', {
-            displayName: 'Community Board Review',
-            dcpMilestonesequence: 48,
-            milestonename: 'Community Board Review',
-            dcpMilestone: '923beec4-dad0-e711-8116-1458d04e2fb8',
-            milestoneLinks: [],
-            dcpMilestoneoutcome: null,
-            displayDate2: null,
-            displayDate: '2019-11-17T20:31:57.956Z',
-            statuscode: 'Not Started',
-            dcpActualenddate: null,
-            dcpActualstartdate: null,
-            dcpPlannedcompletiondate: null,
-            dcpPlannedstartdate: '2019-11-17T20:31:57.956Z',
-            id: '12',
-          }),
-          server.create('milestone', {
-            displayName: 'Borough Board Review',
-            dcpMilestonesequence: 50,
-            milestonename: 'Borough Board Review',
-            dcpMilestone: '963beec4-dad0-e711-8116-1458d04e2fb8',
-            milestoneLinks: [],
-            dcpMilestoneoutcome: null,
-            displayDate2: '2019-11-06T20:31:58.519Z',
-            displayDate: '2019-10-07T19:31:58.519Z',
-            statuscode: 'In Progress',
-            dcpActualenddate: '2019-11-06T20:31:58.519Z',
-            dcpActualstartdate: '2019-10-07T19:31:58.519Z',
-            dcpPlannedcompletiondate: null,
-            dcpPlannedstartdate: null,
-            id: '38',
-          }),
-        ],
-      }),
+      actions: [
+        server.create('action', { id: '1', dcpName: 'Zoning Special Permit', dcpUlurpnumber: 'C780076TLK' }),
+      ],
+      milestones: [
+        server.create('milestone', {
+          displayName: 'Land Use Application Filed',
+          dcpMilestonesequence: 26,
+          milestonename: 'Land Use Application Filed',
+          dcpMilestone: '663beec4-dad0-e711-8116-1458d04e2fb8',
+          dcpMilestoneoutcome: null,
+          displayDate2: null,
+          displayDate: '2019-07-13T19:31:57.763Z',
+          statuscode: 'Completed',
+          dcpActualenddate: null,
+          dcpActualstartdate: '2019-07-13T19:31:57.763Z',
+          dcpPlannedcompletiondate: null,
+          dcpPlannedstartdate: null,
+          id: '1',
+        }),
+        server.create('milestone', {
+          displayName: 'Application Reviewed at City Planning Commission Review Session',
+          dcpMilestonesequence: 46,
+          milestonename: 'Application Reviewed at City Planning Commission Review Session',
+          dcpMilestone: '8e3beec4-dad0-e711-8116-1458d04e2fb8',
+          milestoneLinks: [],
+          dcpMilestoneoutcome: null,
+          displayDate2: null,
+          displayDate: '2019-10-18T19:31:57.916Z',
+          statuscode: 'Not Started',
+          dcpActualenddate: null,
+          dcpActualstartdate: '2019-10-18T19:31:57.916Z',
+          dcpPlannedcompletiondate: null,
+          dcpPlannedstartdate: null,
+          id: '11',
+        }),
+        server.create('milestone', {
+          displayName: 'Community Board Review',
+          dcpMilestonesequence: 48,
+          milestonename: 'Community Board Review',
+          dcpMilestone: '923beec4-dad0-e711-8116-1458d04e2fb8',
+          milestoneLinks: [],
+          dcpMilestoneoutcome: null,
+          displayDate2: '2019-10-29T20:31:57.956Z',
+          displayDate: '2019-10-05T20:31:57.956Z',
+          statuscode: 'Not Started',
+          dcpActualenddate: null,
+          dcpActualstartdate: null,
+          dcpPlannedcompletiondate: null,
+          dcpPlannedstartdate: '2019-11-17T20:31:57.956Z',
+          id: '12',
+        }),
+        server.create('milestone', {
+          displayName: 'Borough Board Review',
+          dcpMilestonesequence: 50,
+          milestonename: 'Borough Board Review',
+          dcpMilestone: '963beec4-dad0-e711-8116-1458d04e2fb8',
+          milestoneLinks: [],
+          dcpMilestoneoutcome: null,
+          displayDate2: '2019-11-06T20:31:58.519Z',
+          displayDate: '2019-10-07T19:31:58.519Z',
+          statuscode: 'In Progress',
+          dcpActualenddate: '2019-11-06T20:31:58.519Z',
+          dcpActualstartdate: '2019-10-07T19:31:58.519Z',
+          dcpPlannedcompletiondate: null,
+          dcpPlannedstartdate: null,
+          id: '38',
+        }),
+      ],
     });
 
-    await visit('/my-projects/archive');
+    await visit('/projects/5');
 
-    // make sure that milestones list on assignment card shows up
-    assert.ok(find('[data-test-milestone-id="12"]'), 'community board milestone');
-    assert.ok(find('[data-test-milestone-id="38"]'), 'borough board milestone');
+    assert.ok(this.element.querySelector('[data-test-milestone-name="1"]').textContent.includes('Land Use Application Filed'), 'land use app filed milestone');
+    assert.ok(this.element.querySelector('[data-test-milestone-name="11"]').textContent.includes('Application Reviewed at City Planning Commission Review Session'), 'CPC milestone');
+    assert.ok(this.element.querySelector('[data-test-milestone-name="12"]').textContent.includes('Community Board Review'), 'CB milestone');
+    assert.ok(this.element.querySelector('[data-test-milestone-name="38"]').textContent.includes('Borough Board Review'), 'BB milestone');
+
+    assert.ok(this.element.querySelector('[data-test-milestone-dates="1"]').textContent.includes, 'July 13, 2019');
+    assert.ok(this.element.querySelector('[data-test-milestone-dates="11"]').textContent.includes, 'October 18, 2019');
+    assert.ok(this.element.querySelector('[data-test-milestone-dates="12"]').textContent.includes, 'October 5 - October 29, 2019');
+    assert.ok(this.element.querySelector('[data-test-milestone-dates="38"]').textContent.includes, 'October 7 - November 6, 2019');
   });
 });
