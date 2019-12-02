f8d2f6a7057c4e06b5a2168cf08e5c802d71d11c
Matt Gardner <wilburnforce@gmail.com>
2019-12-02 14:56:56 -0500

    Pass QP to server if available

--- app/routes/my-projects/archive.js
+++ app/routes/my-projects/archive.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { get } from '@ember/object';
 
 export default class MyProjectsArchiveRoute extends Route {
   @service
@@ -8,10 +9,13 @@ export default class MyProjectsArchiveRoute extends Route {
   @service
   store;
 
-  async model() {
+  async model(model, transition) {
+    const email = get(transition, 'to.queryParams.email');
+
     // Use this endpoint for now. This will need to be updated when the backend is finalized.
     return this.store.query('assignment', {
       tab: 'archive',
+      ...(email ? { email } : {}),
       include: 'project.milestones,project.dispositions,project.actions,project.dispositions.action,dispositions,dispositions.action',
     }, {
       reload: true,
