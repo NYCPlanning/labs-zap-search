1137bb3b71252a9fd1d8d1b76534e4ff74b10d87
Andy Cochran <acochran@planning.nyc.gov>
2020-02-27 16:24:11 -0500

    update site-message test

--- tests/integration/components/site-message-test.js
+++ tests/integration/components/site-message-test.js
@@ -7,14 +7,9 @@ module('Integration | Component | site-message', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`<SiteMessage />`);
-
     assert.equal(this.element.textContent.trim(), '');
 
-    // Template block usage:
     await render(hbs`
       {{site-message message="template block text"}}
     `);
