b12d4318a6d86846c5e0f52c072b716753716d31
Hannah Kates <hannah.b.kates@gmail.com>
2020-06-06 15:05:35 -0400

    delete unused factory

--- client/mirage/factories/project.js
+++ client/mirage/factories/project.js
@@ -136,15 +136,7 @@ export default Factory.extend({
     return faker.random.number();
   },
 
-  // dcpCommunitydistrict() {
-  //   return faker.random.word();
-  // },
-
   dcpValidatedcommunitydistricts: 'X08;R01;R02;R03',
-
-  // dcpValidatedcommunitydistricts() {
-  //   return faker.random.word();
-  // },
 
   bblFeaturecollection() {
     return bblFeatureCollection;
