d20dcf8e16ace12a496930c0dc0f19f00dd94e02
Hannah Kates <hannah.b.kates@gmail.com>
2020-06-07 12:40:57 -0400

    1153: revise logic for lup tab assignments

--- server/src/assignment/assignment.service.ts
+++ server/src/assignment/assignment.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { OdataService, overwriteCodesWithLabels } from '../odata/odata.service';
-import { 
+import {
   all,
   any,
   comparisonOperator,
-  containsAnyOf 
+  containsAnyOf
 } from '../odata/odata.module';
-import { transformMilestones } from '../project/_utils/transform-milestones';
 import { transformActions } from '../project/_utils/transform-actions';
 
 @Injectable()
@@ -75,21 +74,11 @@ export function transformIntoAssignments(projects, contactid) {
     }
   });
 
-  // this needs to happen before value-mapping because transformMilestones requires raw ids
-  // of nested related references.
-  const projectsWithFixedMilestones = projectsWithFlaggedDispositions.map(project => {
-    return {
-      ...project,
-      dcp_dcp_project_dcp_projectmilestone_project: transformMilestones(project.dcp_dcp_project_dcp_projectmilestone_project, project),
-    }
-  });
-
   // this happens first bc expanded entities provide labelled properties
   // differently
   // TODO: this is screwing up milestones bc the ids need to be known
   // before they're replaced
   const valueMappedProjects = overwriteCodesWithLabels(
-    projectsWithFixedMilestones,
     FIELD_LABEL_REPLACEMENT_WHITELIST,
   );
 
@@ -102,7 +91,6 @@ export function transformIntoAssignments(projects, contactid) {
     return dcp_dcp_project_dcp_projectlupteam_project.map(lupteam => {
       const tab = computeStatusTab(project, lupteam);
       const actions = transformActions(project.dcp_dcp_project_dcp_projectaction_project);
-      const milestones = project.dcp_dcp_project_dcp_projectmilestone_project;
       const dispositions = project.dcp_dcp_project_dcp_communityboarddisposition_project;
       const userDispositions = dispositions
         // filter all dispositions so they're scoped to the user only
@@ -140,46 +128,8 @@ export function transformIntoAssignments(projects, contactid) {
 
 function generateAssignmentsQueryObject(query) {
   const { contactid } = query;
-  const DISPLAY_MILESTONE_IDS = [
-    '963beec4-dad0-e711-8116-1458d04e2fb8',
-    '943beec4-dad0-e711-8116-1458d04e2fb8',
-    '763beec4-dad0-e711-8116-1458d04e2fb8',
-    'a63beec4-dad0-e711-8116-1458d04e2fb8',
-    '923beec4-dad0-e711-8116-1458d04e2fb8',
-    '9e3beec4-dad0-e711-8116-1458d04e2fb8',
-    'a43beec4-dad0-e711-8116-1458d04e2fb8',
-    '863beec4-dad0-e711-8116-1458d04e2fb8',
-    '7c3beec4-dad0-e711-8116-1458d04e2fb8',
-    '7e3beec4-dad0-e711-8116-1458d04e2fb8',
-    '883beec4-dad0-e711-8116-1458d04e2fb8',
-    '783beec4-dad0-e711-8116-1458d04e2fb8',
-    'aa3beec4-dad0-e711-8116-1458d04e2fb8',
-    '823beec4-dad0-e711-8116-1458d04e2fb8',
-    '663beec4-dad0-e711-8116-1458d04e2fb8',
-    '6a3beec4-dad0-e711-8116-1458d04e2fb8',
-    'a83beec4-dad0-e711-8116-1458d04e2fb8',
-    '843beec4-dad0-e711-8116-1458d04e2fb8',
-    '8e3beec4-dad0-e711-8116-1458d04e2fb8',
-    '780593bb-ecc2-e811-8156-1458d04d0698',
-
-    // these are study area entities and
-    // TODO: need to also check for study
-    // area flag
-    '483beec4-dad0-e711-8116-1458d04e2fb8',
-    '4a3beec4-dad0-e711-8116-1458d04e2fb8',
-  ];
-  const MILESTONES_FILTER = all(
-    `(not ${comparisonOperator('statuscode', 'eq', 717170001)})`,
-    containsAnyOf('_dcp_milestone_value', DISPLAY_MILESTONE_IDS, {
-      comparisonStrategy: (prop, val) => comparisonOperator(prop, 'eq', val),
-    }),
-  );
   const DISPOSITIONS_FILTER = all(
-    any(
-      comparisonOperator('dcp_visibility', 'eq', 717170004),
-      comparisonOperator('dcp_visibility', 'eq', 717170003),
-    ),
-    `(not ${comparisonOperator('statuscode', 'eq', 717170001)})`,
+    `(not ${comparisonOperator('statuscode', 'eq', 717170001)})`, // not deactivated
   );
 
   return {
@@ -194,10 +144,9 @@ function generateAssignmentsQueryObject(query) {
         and dcp_dcp_project_dcp_projectlupteam_project/any(o:o/statuscode eq 1)
     `,
 
-    // TODO: dispositions need these: AND disp.dcp_visibility IN ('General Public', 'LUP') AND disp.statuscode <> 'Deactivated'
     $expand: `
       dcp_dcp_project_dcp_communityboarddisposition_project($filter=${DISPOSITIONS_FILTER}),
-      dcp_dcp_project_dcp_projectmilestone_project($filter=${MILESTONES_FILTER};$select=dcp_milestone,dcp_name,dcp_plannedstartdate,dcp_plannedcompletiondate,dcp_actualstartdate,dcp_actualenddate,statuscode,dcp_milestonesequence,dcp_remainingplanneddayscalculated,dcp_remainingplanneddays,dcp_goalduration,dcp_actualdurationasoftoday,_dcp_milestone_value,_dcp_milestoneoutcome_value),
+      dcp_dcp_project_dcp_projectmilestone_project;$select=dcp_milestone,dcp_name,dcp_plannedstartdate,dcp_plannedcompletiondate,dcp_actualstartdate,dcp_actualenddate,statuscode,dcp_milestonesequence,dcp_remainingplanneddayscalculated,dcp_remainingplanneddays,dcp_goalduration,dcp_actualdurationasoftoday,_dcp_milestone_value,_dcp_milestoneoutcome_value),
       dcp_dcp_project_dcp_projectaction_project($select=_dcp_action_value,dcp_name,statuscode,statecode,dcp_ulurpnumber,_dcp_zoningresolution_value,dcp_ccresolutionnumber),
       dcp_dcp_project_dcp_projectbbl_project,
       dcp_dcp_project_dcp_projectlupteam_project($filter=(_dcp_lupteammember_value eq ${contactid}) and (statuscode eq 1))
@@ -208,16 +157,8 @@ function generateAssignmentsQueryObject(query) {
 // TODO: finish this — currently defaults to "to review"!
 function computeStatusTab(project, lupteam) {
   const {
-    dcp_dcp_project_dcp_projectmilestone_project: projectMilestones,
     dcp_dcp_project_dcp_communityboarddisposition_project: dispositions,
   } = project;
-
-  // retrieve the LUP's relevant projectMilestones
-  const participantProjectMilestones = projectMilestones.filter(
-    milestone => (milestone.dcp_milestone === '923beec4-dad0-e711-8116-1458d04e2fb8' && lupteam.dcp_lupteammemberrole === 'CB')
-      || (milestone.dcp_milestone === '943beec4-dad0-e711-8116-1458d04e2fb8' && lupteam.dcp_lupteammemberrole === 'BP')
-      || (milestone.dcp_milestone === '963beec4-dad0-e711-8116-1458d04e2fb8' && lupteam.dcp_lupteammemberrole === 'BB')
-    );
 
   const participantDispositions = dispositions.filter(
     disposition => disposition.dcp_representing === 'Borough President' && lupteam.dcp_lupteammemberrole === 'BP'
@@ -229,21 +170,32 @@ function computeStatusTab(project, lupteam) {
     return 'archive';
   }
 
-  if (participantProjectMilestones.find(milestone => milestone.statuscode === 'Not Started')) {
-    return 'upcoming';
-  }
-
   // TODO: this is sensitive to sort order. we need to revise this!!!
   if (
-    participantProjectMilestones.find(milestone => ['In Progress', 'Completed'].includes(milestone.statuscode))
-      && participantDispositions.find(disposition => ['Inactive'].includes(disposition.statecode) && ['Submitted', 'Not Submitted'].includes(disposition.statuscode))) {
+    participantDispositions.find(disposition =>
+      ['General Public', 'LUP'].includes(disposition.dcp_visibility)
+      && ['Submitted', 'Not Submitted'].includes(disposition.statuscode)
+      && ['Inactive'].includes(disposition.statecode))
+    ) {
     return 'reviewed';
   }
 
   if (
-    participantProjectMilestones.find(milestone => ['In Progress', 'Completed'].includes(milestone.statuscode))
-      && participantDispositions.find(disposition => ['Active'].includes(disposition.statecode) && ['Draft', 'Saved'].includes(disposition.statuscode))) {
+    participantDispositions.find(disposition =>
+      ['General Public', 'LUP'].includes(disposition.dcp_visibility)
+      && ['Draft', 'Saved'].includes(disposition.statuscode)
+      && ['Active'].includes(disposition.statecode))
+    ) {
     return 'to-review';
+  }
+
+  if (
+    participantDispositions.find(disposition =>
+      !disposition.dcp_visibility // upcoming if dcp_visibility is null
+      && ['Draft'].includes(disposition.statuscode)
+      && ['Active'].includes(disposition.statecode))
+    ) {
+    return 'upcoming';
   }
 
   return null;
