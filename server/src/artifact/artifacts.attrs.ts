dcb7f48ea8569262ee7099019bf8972093734267
godfreyyeung <godfreyyeung@gmail.com>
2020-11-30 11:03:17 -0800

    13237 Backend for listing project artifacts w docs

---
+++ server/src/artifact/artifacts.attrs.ts
@@ -0,0 +1,11 @@
+export const ARTIFACT_ATTRS = [
+  'dcp_artifactdocumentlocation',
+  'versionnumber',
+  'dcp_submittedfilename',
+  'dcp_artifactsid',
+  'dcp_name',
+  'modifiedon',
+  'dcp_packagetype',
+  'dcp_visibility',
+  'documents',
+];
