246e1de4396d7bac69820fbd4e8da1dc888f22b4
godfreyyeung <godfreyyeung@gmail.com>
2021-01-28 13:38:23 -0800

    Replace MilestoneConstants service with simple ES6 exports

--- client/app/components/archive-project-card.js
+++ client/app/components/archive-project-card.js
@@ -1,9 +1,5 @@
 import Component from '@ember/component';
-import { inject as service } from '@ember/service';
 
 export default class ReviewedProjectCardComponent extends Component {
-  @service
-  milestoneConstants;
-
   assignment = {};
 }
