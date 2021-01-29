360f925c801075195303486d9f4aa2899c3fb6d2
godfreyyeung <godfreyyeung@gmail.com>
2021-01-28 16:56:00 -0800

    788 Convert dateDisplay(2) to CP on frontend Milestone model

--- client/app/models/milestone.js
+++ client/app/models/milestone.js
@@ -1,5 +1,35 @@
 import DS from 'ember-data';
 import { computed } from '@ember/object';
+import {
+  BOROUGH_BOARD_REFERRAL,
+  BOROUGH_PRESIDENT_REFERRAL,
+  CEQR_FEE_PAYMENT,
+  CITY_COUNCIL_REVIEW,
+  COMMUNITY_BOARD_REFERRAL,
+  CPC_PUBLIC_MEETING_PUBLIC_HEARING,
+  CPC_PUBLIC_MEETING_VOTE,
+  CPC_REVIEW_OF_COUNCIL_MODIFICATION,
+  DEIS_PUBLIC_HEARING_HELD,
+  DEIS_SCOPE_OF_WORK_RELEASED,
+  EIS_DRAFT_SCOPE_REVIEW,
+  EIS_PUBLIC_SCOPING_MEETING,
+  FEIS_SUBMITTED_AND_REVIEW,
+  FILED_EAS_REVIEW,
+  FINAL_LETTER_SENT,
+  FINAL_SCOPE_OF_WORK_ISSUED,
+  LAND_USE_FEE_PAYMENT,
+  MAYORAL_VOTE,
+  NOC_OF_DRAFT_EIS_ISSUED,
+  PREPARE_FILED_LAND_USE_APPLICATION,
+  REVIEW_SESSION_CERTIFIED_REFERRED,
+  REVIEW_SESSION_POST_HEARING_FOLLOW_UP_FUTURE_VOTES,
+  REVIEW_SESSION_PRE_HEARING_REVIEW_POST_REFERRAL,
+  SCOPING_MEETING,
+  STATUSCODE_OPTIONSET,
+} from './milestone/constants';
+import {
+  DCPPUBLICSTATUS_OPTIONSET,
+} from './project/constants';
 
 const {
   Model, attr, belongsTo,
@@ -44,6 +74,8 @@ export default class MilestoneModel extends Model {
   // --> CRM:dcp_actualenddate | e.g. '2018-05-12T04:00:00'
   @attr('string') dcpActualenddate;
 
+  @attr('string') dcpReviewmeetingdate;
+
   // --> CRM:statuscode | e.g. 'Not Started', 'In Progress', 'Completed', 'Overridden'
   @attr('string') statuscode;
 
@@ -55,12 +87,6 @@ export default class MilestoneModel extends Model {
 
   // --> CRM:display_name | e.g. 'Land Use Fee Payment'
   @attr('string') displayName;
-
-  // --> CRM:display_date | e.g. '2018-10-31T01:21:46'
-  @attr('string') displayDate;
-
-  // --> CRM:display_date_2 | e.g. null
-  @attr('string') displayDate2;
 
   // field `dcp_remainingplanneddayscalculated` from table dcp_projectmilestone
   // represents number of days e.g. `8`
@@ -79,9 +105,9 @@ export default class MilestoneModel extends Model {
   // In list of milestones with same displayName,
   // each milestone AFTER the first will have `Revised` before the name (isRevised === true)
   // milestones are already sorted in the backend by date
-  @computed('project.milestones', 'id', 'displayName')
+  @computed('project.sortedMilestones', 'id', 'displayName')
   get isRevised() {
-    const projectMilestones = this.get('project.milestones');
+    const projectMilestones = this.get('project.sortedMilestones');
     const currentMilestoneList = projectMilestones.filter(m => m.displayName === this.displayName);
     // check if the current milestone id matches the first in the list
     return this.id !== currentMilestoneList.firstObject.id;
@@ -106,5 +132,86 @@ export default class MilestoneModel extends Model {
 
     // get date diff by accounting for DST
     return getTimeDiffText(remainingTimeInterval);
+  }
+
+  @computed('project.dcpPublicstatus', 'dcpMilestone', 'dcpActualstartdate', 'dcpActualenddate', 'dcpReviewmeetingdate')
+  get displayDate() {
+    const projectPublicStatus = this.project.get('dcpPublicstatus');
+
+    let displayDate = null;
+
+    if ([
+      BOROUGH_BOARD_REFERRAL,
+      BOROUGH_PRESIDENT_REFERRAL,
+      COMMUNITY_BOARD_REFERRAL,
+      CITY_COUNCIL_REVIEW,
+      CPC_PUBLIC_MEETING_PUBLIC_HEARING,
+      EIS_DRAFT_SCOPE_REVIEW,
+      FEIS_SUBMITTED_AND_REVIEW,
+      MAYORAL_VOTE,
+    ].includes(this.dcpMilestone) && projectPublicStatus !== DCPPUBLICSTATUS_OPTIONSET.FILED) {
+      displayDate = this.dcpActualstartdate;
+    }
+
+    if (this.dcpMilestone === FILED_EAS_REVIEW) {
+      displayDate = this.dcpActualstartdate;
+    }
+
+    if ([
+      FINAL_LETTER_SENT,
+      FINAL_SCOPE_OF_WORK_ISSUED,
+      CPC_PUBLIC_MEETING_VOTE,
+      DEIS_PUBLIC_HEARING_HELD,
+      NOC_OF_DRAFT_EIS_ISSUED,
+      REVIEW_SESSION_CERTIFIED_REFERRED,
+    ].includes(this.dcpMilestone) && projectPublicStatus !== DCPPUBLICSTATUS_OPTIONSET.FILED) {
+      displayDate = this.dcpActualenddate;
+    }
+
+    if ([
+      EIS_PUBLIC_SCOPING_MEETING,
+      PREPARE_FILED_LAND_USE_APPLICATION,
+      LAND_USE_FEE_PAYMENT,
+      CEQR_FEE_PAYMENT,
+      CPC_REVIEW_OF_COUNCIL_MODIFICATION,
+      DEIS_SCOPE_OF_WORK_RELEASED,
+      SCOPING_MEETING,
+    ].includes(this.dcpMilestone)) {
+      displayDate = this.dcpActualenddate;
+    }
+
+    if ([
+      REVIEW_SESSION_CERTIFIED_REFERRED, // aka 'Application Reviewed at City Planning Commission Review Session'
+      CPC_PUBLIC_MEETING_VOTE, // aka 'City Planning Commission Vote'
+      CPC_PUBLIC_MEETING_PUBLIC_HEARING, // aka 'CPC Public Meeting - Public Hearing' a.k.a 'City Planning Commission Review'
+      REVIEW_SESSION_PRE_HEARING_REVIEW_POST_REFERRAL, // aka 'Review Session - Pre-Hearing Review / Post Referral'
+      REVIEW_SESSION_POST_HEARING_FOLLOW_UP_FUTURE_VOTES, // aka 'Review Session - Post Hearing Follow-Up / Future Votes'
+    ].includes(this.dcpMilestone)) {
+      if ((this.statuscode === STATUSCODE_OPTIONSET.COMPLETED) || (this.statuscode === STATUSCODE_OPTIONSET.IN_PROGRESS)) {
+        if ((this.dcpReviewmeetingdate) && (Date.now() >= this.dcpReviewmeetingdate.getTime())) {
+          displayDate = this.dcpReviewmeetingdate;
+        }
+      }
+    }
+
+    return displayDate;
+  }
+
+  @computed('project.dcpPublicstatus', 'dcpMilestone', 'dcpActualenddate', 'dcpPlannedcompletiondate')
+  get displayDate2() {
+    const projectPublicStatus = this.project.get('dcpPublicstatus');
+
+    if ([
+      BOROUGH_BOARD_REFERRAL,
+      BOROUGH_PRESIDENT_REFERRAL,
+      COMMUNITY_BOARD_REFERRAL,
+      CITY_COUNCIL_REVIEW,
+      CPC_PUBLIC_MEETING_PUBLIC_HEARING,
+      MAYORAL_VOTE,
+    ].includes(this.dcpMilestone) && projectPublicStatus !== DCPPUBLICSTATUS_OPTIONSET.FILED) {
+      return this.dcpActualenddate || this.dcpPlannedcompletiondate;
+    }
+
+    return null;
   }
 }
