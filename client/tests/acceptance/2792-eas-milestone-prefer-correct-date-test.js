f6e457ffcf4d4ee0e87ec2ec09ac11f011c19b40
allthesignals <>
2021-05-27 16:13:02 -0400

    2792: add acceptance tests

---
+++ client/tests/acceptance/2792-eas-milestone-prefer-correct-date-test.js
@@ -0,0 +1,47 @@
+import { module, test } from 'qunit';
+import { visit, find } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+
+module('Acceptance | 2792 eas milestone prefer correct date', function(hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  test('eas prefer correct date for projects beginning with P', async function(assert) {
+    this.server.create('project', {
+      dcpName: 'P1234',
+      milestones: [
+        this.server.create('milestone', 'isCompleted', 'filedEasReview', {
+          id: 1,
+          dcpActualstartdate: '9/9/9',
+        }),
+        this.server.create('milestone', 'isCompleted', {
+          dcpMilestone: '723beec4-dad0-e711-8116-1458d04e2fb8', // prepared eas
+          dcpActualenddate: '1/1/1',
+        }),
+      ],
+    });
+    await visit('/projects/1');
+
+    assert.equal(find('[data-test-milestone-dates="1"').textContent.trim(), 'January 1, 2001');
+  });
+
+  test('eas prefer correct date for projects beginning withOUT P', async function(assert) {
+    this.server.create('project', {
+      dcpName: '1234',
+      milestones: [
+        this.server.create('milestone', 'isCompleted', 'filedEasReview', {
+          id: 1,
+          dcpActualstartdate: '9/9/9',
+        }),
+        this.server.create('milestone', 'isCompleted', {
+          dcpMilestone: '723beec4-dad0-e711-8116-1458d04e2fb8', // prepared eas
+          dcpActualenddate: '1/1/1',
+        }),
+      ],
+    });
+    await visit('/projects/1');
+
+    assert.equal(find('[data-test-milestone-dates="1"').textContent.trim(), 'September 9, 2009');
+  });
+});
