399b7e5a67436d001ab036670ee9545bb6db3144
jlhogan <julia@skylight.digital>
2021-06-13 20:44:44 -0400

    Remove queryFromObjectReferences except assignment controller

--- server/src/contact/contact.service.ts
+++ server/src/contact/contact.service.ts
@@ -1,39 +1,33 @@
-import {
-  Injectable,
-} from '@nestjs/common';
-import {
-  CrmService,
-} from '../crm/crm.service';
-import {
-  all,
-  comparisonOperator,
-} from '../crm/crm.utilities';
+import { Injectable } from "@nestjs/common";
+import { CrmService } from "../crm/crm.service";
+import buildQuery from "odata-query";
 
 @Injectable()
 export class ContactService {
-  constructor(
-    private readonly crmService: CrmService,
-  ) {}
+  constructor(private readonly crmService: CrmService) {}
 
-  async findOne(contactid: any): Promise<any> {
-    const { records: [contact] } = await this.crmService.queryFromObject('contacts', {
-      $filter: all(
-        comparisonOperator('statuscode', 'eq', 1),
-        comparisonOperator('contactid', 'eq', contactid)
-      ),
-    });
+  async findOne(contactId: any): Promise<any> {
+    const {
+      records: [contact]
+    } = await this.crmService.query(
+      "contacts",
+      buildQuery({
+        filter: [{ statuscode: 1 }, { contactid: contactId }]
+      })
+    );
 
     return contact;
   }
 
-  async findByEmail(email): Promise<any> {
-    const { records: [contact] } = await this.crmService.queryFromObject('contacts', {
-      $filter: all(
-        comparisonOperator('statuscode', 'eq', 1),
-        `startswith(emailaddress1, '${email}')`,
-      ),
-    });
-
+  async findByEmail(email: string): Promise<any> {
+    const {
+      records: [contact]
+    } = await this.crmService.query(
+      "contacts",
+      buildQuery({
+        filter: [{ statuscode: 1 }, `startswith(emailaddress1, '${email}')`]
+      })
+    );
     return contact;
   }
 }
