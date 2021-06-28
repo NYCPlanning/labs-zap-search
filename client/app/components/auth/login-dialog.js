86a8ab9ca1b290d22fdf4617ad7231a21aaa4f22
allthesignals <>
2021-06-28 12:40:31 -0400

    Add message warning of upcoming downtime

--- client/app/components/auth/login-dialog.js
+++ client/app/components/auth/login-dialog.js
@@ -1,6 +1,22 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import ENV from 'labs-zap-search/config/environment';
+
+const MAINTENANCE_RANGE = ['06/28/2021 19:00', '06/29/2021 19:00'];
 
 export default Component.extend({
   oauthEndpoint: ENV.OAUTH_ENDPOINT,
+  isMaintenancePeriod: computed(function() {
+    const [from, to] = MAINTENANCE_RANGE.map(string => new Date(string));
+    const now = new Date();
+
+    return now > from && now < to;
+  }),
+
+  hasUpcomingMaintenance: computed(function() {
+    const [, to] = MAINTENANCE_RANGE.map(string => new Date(string));
+    const now = new Date();
+
+    return now < to;
+  }),
 });
