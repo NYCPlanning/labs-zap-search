d2511a1e853d21d0aa0f06fd0d2dc05a4f78cc48
horatio <horatiorosa@aol.com>
2022-07-06 09:08:39 -0400

    1693 changed isTextTooLong method to return boolean

--- client/app/helpers/is-text-too-lengthy.js
+++ client/app/helpers/is-text-too-lengthy.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
 
 export function isTextTooLengthy(text) {
-  return (text.length && text.length > 22) ? 'center-text-tiny' : '';
+  return (text.length && text.length > 22);
 }
 
 export default helper(isTextTooLengthy);
