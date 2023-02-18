f9b2fe731fb1cf7885dea8656e649af88cffb113
Tyler Matteo <tylermatteo434@gmail.com>
2023-02-18 12:44:53 -0500

    Set up basic Sentry integration

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,18 +1,20 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
-import * as bodyParser from 'body-parser';
-import * as cookieparser from 'cookie-parser';
-import * as compression from 'compression';
-import { AuthMiddleware } from './auth.middleware';
-import { ConfigModule } from './config/config.module';
-import { ProjectModule } from './project/project.module';
-import { AppController } from './app.controller';
-import { ContactModule } from './contact/contact.module';
-import { AuthModule } from './auth/auth.module';
-import { DispositionModule } from './disposition/disposition.module';
-import { AssignmentModule } from './assignment/assignment.module';
-import { DocumentModule } from './document/document.module';
-import { CrmModule } from './crm/crm.module';
-import { ZoningResolutionsModule } from './zoning-resolutions/zoning-resolutions.module';
+import { Module, NestModule, MiddlewareConsumer } from "@nestjs/common";
+import { APP_INTERCEPTOR } from "@nestjs/core";
+import { RavenModule, RavenInterceptor } from "nest-raven";
+import * as bodyParser from "body-parser";
+import * as cookieparser from "cookie-parser";
+import * as compression from "compression";
+import { AuthMiddleware } from "./auth.middleware";
+import { ConfigModule } from "./config/config.module";
+import { ProjectModule } from "./project/project.module";
+import { AppController } from "./app.controller";
+import { ContactModule } from "./contact/contact.module";
+import { AuthModule } from "./auth/auth.module";
+import { DispositionModule } from "./disposition/disposition.module";
+import { AssignmentModule } from "./assignment/assignment.module";
+import { DocumentModule } from "./document/document.module";
+import { CrmModule } from "./crm/crm.module";
+import { ZoningResolutionsModule } from "./zoning-resolutions/zoning-resolutions.module";
 
 @Module({
   imports: [
@@ -25,27 +27,30 @@ import { ZoningResolutionsModule } from './zoning-resolutions/zoning-resolutions
     AssignmentModule,
     DocumentModule,
     ZoningResolutionsModule,
-   ],
+    RavenModule
+  ],
   controllers: [AppController],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useValue: new RavenInterceptor()
+    }
+  ]
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(cookieparser())
-      .forRoutes('*');
+    consumer.apply(cookieparser()).forRoutes("*");
+
+    consumer.apply(AuthMiddleware).forRoutes("*");
 
     consumer
-      .apply(AuthMiddleware)
-      .forRoutes('*');
+      .apply(
+        bodyParser.json({
+          type: "application/vnd.api+json"
+        })
+      )
+      .forRoutes("*");
 
-    consumer
-      .apply(bodyParser.json({
-        type: 'application/vnd.api+json'
-      }))
-      .forRoutes('*');
-
-    consumer
-      .apply(compression())
-      .forRoutes('*');
+    consumer.apply(compression()).forRoutes("*");
   }
 }
