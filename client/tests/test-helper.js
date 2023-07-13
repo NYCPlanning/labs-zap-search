b6e47d73cc37dd3955631bd3be3f6aa9916b81f3
Bryan Marchena <bmarchena@planning.nyc.gov>
2023-07-13 11:42:58 -0400

    Update Ember CLI to v. 3.21.2

--- client/tests/test-helper.js
+++ client/tests/test-helper.js
@@ -1,7 +1,7 @@
+import Application from 'labs-zap-search/app';
+import config from 'labs-zap-search/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
-import Application from '../app';
-import config from '../config/environment';
 
 setApplication(Application.create(config.APP));
 
