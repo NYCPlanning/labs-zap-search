f02e31186a0e6f9878c6ab78dee5f27f72ef0b3d
pratishta <ypradhi9@gmail.com>
2024-09-26 11:53:40 -0400

    Update netlify.toml

--- client/config/environment.js
+++ client/config/environment.js
@@ -4,7 +4,7 @@ const { MIRAGE_SCENARIO } = process.env;
 const NYCID_CLIENT_ID = process.env.NYCID_CLIENT_ID || 'lup-portal-local';
 const NYC_ID_HOST =
   process.env.NYC_ID_HOST || 'https://accounts-nonprd.nyc.gov/account';
-const ZAP_ALERT = process.env.ZAP_ALERT || 'ON';
+const SHOW_ALERTS = process.env.SHOW_ALERTS || 'OFF';
 
 module.exports = function (environment) {
   const ENV = {
@@ -18,7 +18,7 @@ module.exports = function (environment) {
     },
     NYC_ID_HOST,
     maintenanceTimes: getMaintenanceTimes(),
-    ZAP_ALERT,
+    SHOW_ALERTS,
     host: getHost(environment),
     OAUTH_ENDPOINT: `${NYC_ID_HOST}/api/oauth/authorize.htm?response_type=token&client_id=${NYCID_CLIENT_ID}`,
     LUPP_ENABLED: true,
