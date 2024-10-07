9cb64daafc0ecf5d94d2d6032530e6cd92b2fca7
David Hochbaum <dhochbaum@planning.nyc.gov>
2024-10-07 13:37:21 -0400

    Added email validation

--- server/src/subscription/subscription.controller.ts
+++ server/src/subscription/subscription.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get, Post, Param, Query, Req, Res } from "@nestjs/common";
+import { error } from "console";
 import { Request } from "express";
+import validateEmail from "../_utils/validate-email";
 
 
 @Controller()
 export class SubscriptionController {
   constructor(
+    
    
   ) {}
 
@@ -12,11 +15,15 @@ export class SubscriptionController {
   @Post("/subscribers")
   async addSubscriber(@Req() request: Request, @Res() response) {
 
-    console.log(request.body);
-
-    response.status(201).send({
-      status: "success",
-    });
+    if (!validateEmail(request.body.email)) {
+      response.status(400).send({
+        error: "Invalid email address."
+      })
+    } else {
+      response.status(201).send({
+        status: "success",
+      });
+    }
 
   }
 }
