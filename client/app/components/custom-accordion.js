831f780a182ff292b94814203d8e67251fa0268d
pratishta <ypradhi9@gmail.com>
2024-10-10 11:53:46 -0400

    custom component

--- client/app/components/custom-accordion.js
+++ client/app/components/custom-accordion.js
@@ -1,6 +1,33 @@
 import Ember from "ember";
+import foundation from "foundation-sites";
+require('foundation-sites');
 export default Ember.Component.extend({
-  didInsertElement: function() {
-    $(document).foundation(); // this will only load the section component
-  }
+  initFoundation: Ember.on('didInsertElement', function () {
+    Ember.run.next(() => {
+      Ember.$(document).jquery(foundation)
+    });
+  })
+  // initAccordion: function() {
+  //   this.$(document).foundation('accordion', {
+      
+  //   });
+  //   console.log("sldkj");
+
+  // }.on('didInsertElement')
+  
+  // didInsertElement() {
+  //   // this._super(...arguments);
+  //   console.log("sldkfj");
+  //   Ember.run.scheduleOnce('didInsertElement', this, function() {
+  //     this.$().foundation('accordion', 'reflow');
+  //   });
+  //   // this.$().foundation(
+  //   //   'accordion'
+  //   // )
+  // },
+  // didInsertElement() {
+  //   console.log("hi");
+  //   Ember.$(document).foundation();
+  // }
+ 
 });
