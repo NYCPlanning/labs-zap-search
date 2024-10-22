c70515c61b660965762a6bab1796897c71c09de0
pratishta <ypradhi9@gmail.com>
2024-10-22 15:28:42 -0400

    Add component for subscription form

---
+++ client/app/components/subscription-form.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+import { action } from '@ember/object';
+import { run } from '@ember/runloop';
+
+export default class SubscriptionFormComponent extends Component {
+    
+}
