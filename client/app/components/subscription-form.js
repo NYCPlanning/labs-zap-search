cbb70024d7560a83855363fa6184e57869f076cb
pratishta <ypradhi9@gmail.com>
2024-10-23 16:21:07 -0400

    Make select all checkbox conditional

--- client/app/components/subscription-form.js
+++ client/app/components/subscription-form.js
@@ -4,4 +4,24 @@ import { run } from '@ember/runloop';
 
 export default class SubscriptionFormComponent extends Component {
     
+    @action
+    handleCityWideChecked() {
+        this.set('')
+    }
+
+    @action
+    checkWholeBorough(newValue) {
+        const checkboxId = newValue.target.id;
+        if (checkboxId === "all-brooklyn") {
+            this.set("allBrooklyn", newValue.target.checked) 
+        } else if (checkboxId === "all-bronx") {
+            this.set("allBronx", newValue.target.checked) 
+        } else if (checkboxId === "all-manhattan") {
+            this.set("allManhattan", newValue.target.checked) 
+        } else if (checkboxId === "all-queens") {
+            this.set("allQueens", newValue.target.checked) 
+        } else if (checkboxId === "all-staten-island") {
+            this.set("allStatenIsland", newValue.target.checked) 
+        }
+    }
 }
