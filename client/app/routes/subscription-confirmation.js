bd9e2a633717da83b05af82e6421f1431dd5746a
David Hochbaum <dhochbaum@planning.nyc.gov>
2024-11-14 10:40:09 -0500

    Fixed linting errors.

--- client/app/routes/subscription-confirmation.js
+++ client/app/routes/subscription-confirmation.js
@@ -4,43 +4,44 @@ import ENV from 'labs-zap-search/config/environment';
 
 export default Route.extend({
   convertSubscriptionsToHandlebars(subscriptions) {
-    var handlebars = { "citywide": false, "boroughs": [] }
+    const handlebars = { citywide: false, boroughs: [] };
     const boros = {
-      "K": "Brooklyn",
-      "X": "Bronx",
-      "M": "Manhattan",
-      "Q": "Queens",
-      "R": "Staten Island"
-    }
+      K: 'Brooklyn',
+      X: 'Bronx',
+      M: 'Manhattan',
+      Q: 'Queens',
+      R: 'Staten Island',
+    };
+    // eslint-disable-next-line no-restricted-syntax
     for (const [key, value] of Object.entries(subscriptions)) {
       if (value === 1) {
-        if (key === "CW") {
+        if (key === 'CW') {
           handlebars.citywide = true;
         } else if (boros[key[0]]) {
-          const i = handlebars.boroughs.findIndex((boro) => boro.name === boros[key[0]]);
-            if (i === -1) {
-              handlebars.boroughs.push({
-                "name": boros[key[0]],
-                "communityBoards": [parseInt(key.slice(-2))]
-              })
-            } else {
-              handlebars.boroughs[i]["communityBoards"].push(parseInt(key.slice(-2)))
-            }
+          const i = handlebars.boroughs.findIndex(boro => boro.name === boros[key[0]]);
+          if (i === -1) {
+            handlebars.boroughs.push({
+              name: boros[key[0]],
+              communityBoards: [parseInt(key.slice(-2), 10)],
+            });
+          } else {
+            handlebars.boroughs[i].communityBoards.push(parseInt(key.slice(-2), 10));
+          }
         }
       }
     }
     return handlebars;
   },
-  
+
   async model({ id }) {
     // No need to await for this to finish before making the next request
     fetch(`${ENV.host}/subscribers/${id}`, {
       method: 'PATCH',
       headers: {
-      'Content-Type': 'application/json',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ subscriptions: { confirmed: 1 } }),
-    })
+    });
 
     // Go directly into getting the subscriptions
     const response = await fetch(`${ENV.host}/subscribers/${id}`);
@@ -49,5 +50,5 @@ export default Route.extend({
     if (!response.ok) throw await response.json();
 
     return this.convertSubscriptionsToHandlebars(body);
-  }
+  },
 });
