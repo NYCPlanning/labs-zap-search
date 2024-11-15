410a691062f72c737fca206437aa8ed735be9e8f
Tyler Matteo <tylermatteo434@gmail.com>
2024-11-15 11:51:27 -0500

    Move code to build CD dictionary to helper and address most linting violations

--- client/app/components/initialize-foundation.js
+++ client/app/components/initialize-foundation.js
@@ -1,7 +1,8 @@
+import $ from 'jquery';
 import Component from '@ember/component';
 
 export default Component.extend({
-    didRender: function() {
-        $(this.element).foundation();
-    },
+  didRender: function() {
+    $(this.element).foundation();
+  },
 });
--- client/app/components/subscription-form.js
+++ client/app/components/subscription-form.js
@@ -1,34 +1,28 @@
+import $ from 'jquery';
 import Component from '@glimmer/component';
 import { action, set } from '@ember/object';
-import { lookupCommunityDistrict } from '../helpers/lookup-community-district';
-import { tracked } from '@glimmer/tracking';
+import { getCommunityDistrictsByBorough } from '../helpers/lookup-community-district';
 
 export default class SubscriptionFormComponent extends Component {
-
     communityDistrictsByBorough = {};
-    isCommunityDistrict = false;
-    constructor(...args) {
-        super(...args);
 
-        const districts = lookupCommunityDistrict();
-        for (const district of districts) {
-            const {code, num, boro} = district;
-            if(boro in this.communityDistrictsByBorough === false) {
-                this.communityDistrictsByBorough[boro] = []
-            }
-            this.communityDistrictsByBorough[boro].push({code, num, boro})
-        }
+    isCommunityDistrict = false;
+
+    constructor(...args) {
+      super(...args);
+
+      this.communityDistrictsByBorough = getCommunityDistrictsByBorough();
     }
 
     @action
     checkWholeBorough(event) {
-        for(const district of this.communityDistrictsByBorough[event.target.value]) {
-            set(this.args.subscriptions, district.code, event.target.checked)
-        }
+      for (const district of this.communityDistrictsByBorough[event.target.value]) {
+        set(this.args.subscriptions, district.code, event.target.checked);
+      }
     }
 
     @action
-    closeAllAccordions(event) {
-        $('.accordion').foundation('up', $('.accordion .accordion-content'));
+    closeAllAccordions() {
+      $('.accordion').foundation('up', $('.accordion .accordion-content'));
     }
 }
