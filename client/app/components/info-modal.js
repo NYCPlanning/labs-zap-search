0b047fa45c7add680ac1c3a4e1ad7805b696917a
Tyler Matteo <tylermatteo434@gmail.com>
2025-03-19 11:18:04 -0400

    Hide ZAP Alerts modal behind Feature Flag
    Update info-modal component to hide when ZAP Alerts feature flag is turned off
    
    closes #1678
    
    Co-authored-by: Tyler Matteo <tylermatteo434@gmail.com>

--- client/app/components/info-modal.js
+++ client/app/components/info-modal.js
@@ -1,9 +1,10 @@
 import Component from '@ember/component';
 import { action } from '@ember/object';
+import ENV from 'labs-zap-search/config/environment';
 
 export default class InfoModalComponent extends Component {
   // We have manually overridden this to hide the modal by default when running tests in tests/index.html
-  open = !window.localStorage.hideMessage;
+  open = ENV.showAlerts === true && window.localStorage.hideMessage !== 'true';
 
   dontShowModalAgain = false;
 
