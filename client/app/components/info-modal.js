b00673a45eb04fc284dc9f1c87d9bc93d9952f54
Tyler Matteo <tylermatteo434@gmail.com>
2025-03-19 11:18:04 -0400

    Update info-modal component to hide when ZAP Alerts feature flag is turned off

--- client/app/components/info-modal.js
+++ client/app/components/info-modal.js
@@ -1,9 +1,10 @@
 import Component from '@ember/component';
 import { action } from '@ember/object';
+import config from '../../config/environment';
 
 export default class InfoModalComponent extends Component {
   // We have manually overridden this to hide the modal by default when running tests in tests/index.html
-  open = !window.localStorage.hideMessage;
+  open = config.showAlerts === true && !window.localStorage.hideMessage;
 
   dontShowModalAgain = false;
 
